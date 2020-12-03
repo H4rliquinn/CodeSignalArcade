dce0901165a0300996fc352603b559f4c11b11bd
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-02 21:28:08 -0500

    Iterate through even and odd length strings

--- JavaScript/CheckPalindrome.js
+++ JavaScript/CheckPalindrome.js
@@ -1,3 +1,9 @@
 function checkPalindrome(inputString) {
-
+    for (x=0;x<parseInt(inputString.length/2);x++){
+        console.log(inputString[x]);
+    }
 }
+
+checkPalindrome("The String");
+console.log();
+checkPalindrome("Odd The String");
