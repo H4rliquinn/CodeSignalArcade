37046186e747980cb63c07048a1e208b152836b3
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-02 21:33:11 -0500

    Add test for palindrome. Add reverse check

--- JavaScript/CheckPalindrome.js
+++ JavaScript/CheckPalindrome.js
@@ -1,9 +1,13 @@
 function checkPalindrome(inputString) {
     for (x=0;x<parseInt(inputString.length/2);x++){
         console.log(inputString[x]);
+        console.log(inputString[inputString.length-x-1]);
     }
 }
 
 checkPalindrome("The String");
 console.log();
 checkPalindrome("Odd The String");
+console.log();
+checkPalindrome("abcdeedcba");
+
