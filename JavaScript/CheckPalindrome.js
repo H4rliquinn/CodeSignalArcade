75a95a6f91976d539f7159eef094181e48e3f8ec
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-03 20:32:53 -0500

    Added Palindrome test. Console Logs removed and tests updated.

--- JavaScript/CheckPalindrome.js
+++ JavaScript/CheckPalindrome.js
@@ -1,13 +1,15 @@
 function checkPalindrome(inputString) {
     for (x=0;x<parseInt(inputString.length/2);x++){
-        console.log(inputString[x]);
-        console.log(inputString[inputString.length-x-1]);
+        if (inputString[x]!==inputString[inputString.length-x-1]){
+            return false;
+        }
     }
+    return true;
 }
+console.log(checkPalindrome("The String"));
+console.log();
+console.log(checkPalindrome("Odd The String"));
+console.log();
+console.log(checkPalindrome("abcdeedcba"));
 
-checkPalindrome("The String");
-console.log();
-checkPalindrome("Odd The String");
-console.log();
-checkPalindrome("abcdeedcba");
 
