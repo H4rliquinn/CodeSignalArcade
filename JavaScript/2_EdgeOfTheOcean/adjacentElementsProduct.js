19ef4dd8e266ea54bbbf721690a1420fdbbdac49
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-08 22:05:39 -0500

    adjacentElementsProduct all tests passed

--- JavaScript/2_EdgeOfTheOcean/adjacentElementsProduct.js
+++ JavaScript/2_EdgeOfTheOcean/adjacentElementsProduct.js
@@ -1,7 +1,16 @@
 function adjacentElementsProduct(inputArray) {
+    let current_max=Number.NEGATIVE_INFINITY;
+    let last_value=null;
     for (x in inputArray){
-        console.log(inputArray[x]);
+        if (last_value !=null){
+            let current_total=last_value*inputArray[x];
+            if (current_total>current_max){
+                current_max=current_total;
+            }
+        }
+        last_value=inputArray[x];
     }
+    return current_max;
 }
 
 adjacentElementsProduct([1,2,3]);
