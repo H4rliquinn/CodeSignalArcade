7726f2cc1a819413bb87076bb96d4944fd26d68c
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-11 13:53:05 -0500

    Set up center line and decending for loop.

--- JavaScript/2_EdgeOfTheOcean/shapeArea.js
+++ JavaScript/2_EdgeOfTheOcean/shapeArea.js
@@ -1,6 +1,12 @@
 function shapeArea(n) {
     //Center line is n+n-1
+    let centerLine=n+n-1;
     //Each level in triangle decreases by 2 down to 1
+    for (let x=centerLine;x>0;x=x-2){
+        console.log(x);
+    }
     //Top and bottom are identical
     //Return Center + 2 * triangle
 }
+
+shapeArea(3);
