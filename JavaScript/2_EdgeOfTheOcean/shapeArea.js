6ed81736f9c66217052030edd85b5404d23c6380
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-11 13:56:32 -0500

    Tests passed

--- JavaScript/2_EdgeOfTheOcean/shapeArea.js
+++ JavaScript/2_EdgeOfTheOcean/shapeArea.js
@@ -2,11 +2,14 @@ function shapeArea(n) {
     //Center line is n+n-1
     let centerLine=n+n-1;
     //Each level in triangle decreases by 2 down to 1
-    for (let x=centerLine;x>0;x=x-2){
-        console.log(x);
+    let triangle=0;
+    for (let x=centerLine-2;x>0;x=x-2){
+        triangle=triangle+x;
     }
     //Top and bottom are identical
     //Return Center + 2 * triangle
+    return centerLine+2*triangle;
 }
-
-shapeArea(3);
+console.log(shapeArea(3));
+console.log(shapeArea(4));
+console.log(shapeArea(10));
