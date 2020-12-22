4d8ad8c75744df0ac2674653937c384a1835234c
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-22 17:27:32 -0500

    Algorithm complete

--- JavaScript/2_EdgeOfTheOcean/MakeArrayConsecutive_2.js
+++ JavaScript/2_EdgeOfTheOcean/MakeArrayConsecutive_2.js
@@ -1,8 +1,11 @@
 function makeArrayConsecutive2(statues) {
-    for (let x=Math.min(...statues);x<Math.max(...statues);x++){
-        console.log(x);
+    let missing=[];
+    for (let x=Math.min(...statues)+1;x<Math.max(...statues);x++){
+        if (!statues.includes(x)){
+            missing.push(x);
+        }
     }
-    return statues;
+    return missing.length;
 }
 
 let statues=[6,2,3,8]
