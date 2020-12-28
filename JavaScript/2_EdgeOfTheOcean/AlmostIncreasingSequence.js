a44346734fbdfb2e4234f13b689858f8a6a9af7b
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-28 14:25:08 -0500

    First pass 11/19 tests passed

--- JavaScript/2_EdgeOfTheOcean/AlmostIncreasingSequence.js
+++ JavaScript/2_EdgeOfTheOcean/AlmostIncreasingSequence.js
@@ -1,3 +1,22 @@
 function almostIncreasingSequence(sequence) {
-
+    let highest=Number.NEGATIVE_INFINITY;
+    let strike=0;
+    for(let x=0;x<sequence.length;x++){
+      if (sequence[x]>=highest){
+        highest=sequence[x];
+      } else  {
+        strike++;
+      }
+      if (strike>1){
+        return false;
+      }
+    }
+    return true;
 }
+
+let sec=[1,3,2,1];
+let sec2=[1,3,2];
+
+console.log(almostIncreasingSequence(sec));
+console.log();
+console.log(almostIncreasingSequence(sec2));
