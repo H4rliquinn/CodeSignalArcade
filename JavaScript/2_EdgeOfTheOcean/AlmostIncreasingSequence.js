142a4252a8da9ae283bb44c6012004e4bc1495a6
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-28 14:32:11 -0500

    Refactored. Passing 13/119 tests

--- JavaScript/2_EdgeOfTheOcean/AlmostIncreasingSequence.js
+++ JavaScript/2_EdgeOfTheOcean/AlmostIncreasingSequence.js
@@ -1,13 +1,14 @@
 function almostIncreasingSequence(sequence) {
     let highest=Number.NEGATIVE_INFINITY;
+    let rollback=Number.NEGATIVE_INFINITY;
     let strike=0;
     for(let x=0;x<sequence.length;x++){
       if (sequence[x]>highest){
+        rollback=highest;
         highest=sequence[x];
-      } else  {
-        strike++;
-      }
-      if (strike>1){
+      } else if (sequence[x]>rollback) {
+        highest=sequence[x];
+      } else {
         return false;
       }
     }
@@ -16,7 +17,10 @@ function almostIncreasingSequence(sequence) {
 
 let sec=[1,3,2,1];
 let sec2=[1,3,2];
+let sec3=[1, 2, 1, 2];
 
 console.log(almostIncreasingSequence(sec));
 console.log();
 console.log(almostIncreasingSequence(sec2));
+console.log();
+console.log(almostIncreasingSequence(sec3));
