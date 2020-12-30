45546387d69dc7cb0a2d924593562bfd99ac37b8
Jason Sonnichsen <Harliquinn@gmail.com>
2020-12-30 13:33:40 -0500

    Tests passed, level complete

--- JavaScript/2_EdgeOfTheOcean/MatrixElementsSum.js
+++ JavaScript/2_EdgeOfTheOcean/MatrixElementsSum.js
@@ -1,5 +1,27 @@
 function matrixElementsSum(matrix) {
+    let total=0;
     //Work through matrix by length first
-    //Work down the height
-    //Stop when you find a ghost
+    for (let x=0;x<matrix[0].length;x++){
+        //Work down the height
+        for (let y=0;y<matrix.length;y++){
+            if (matrix[y][x]>0){
+                total=total+matrix[y][x];
+            } else {
+                //Stop when you find a ghost
+                break;
+            }
+        }
+    }
+    return total;
 }
+
+let matrix = [[0, 1, 1, 2], 
+          [0, 5, 0, 0], 
+          [2, 0, 3, 3]]
+          
+let matrix2 = [[1, 1, 1, 0], 
+          [0, 5, 0, 1], 
+          [2, 1, 3, 10]]  
+
+console.log(matrixElementsSum(matrix));
+console.log(matrixElementsSum(matrix2));
