162c2560815291c2233425c3bc06f52320ee67cf
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-04 13:26:08 -0500

    Library created for reference

--- JavaScript/3_SmoothSailing/AllLongestStrings.js
+++ JavaScript/3_SmoothSailing/AllLongestStrings.js
@@ -2,8 +2,14 @@ function allLongestStrings(inputArray) {
  let library={};
  let longest=0;
  for (let item in inputArray){
-     console.log(inputArray[item]);
+     if (inputArray[item].length in library){
+        library[inputArray[item].length].push(inputArray[item]);
+     }else{
+        library[inputArray[item].length]=[inputArray[item]];
+     }
  }
+
+ console.log(library);
 }
 
 allLongestStrings(["aba", 
