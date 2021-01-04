28224f3e43d08dc4416491f2dc9d42d24114ba63
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-04 12:56:53 -0500

    Iterate through array

--- JavaScript/3_SmoothSailing/AllLongestStrings.js
+++ JavaScript/3_SmoothSailing/AllLongestStrings.js
@@ -1,3 +1,13 @@
 function allLongestStrings(inputArray) {
-
+ let library={};
+ let longest=0;
+ for (let item in inputArray){
+     console.log(inputArray[item]);
+ }
 }
+
+allLongestStrings(["aba", 
+"aa", 
+"ad", 
+"vcd", 
+"aba"]);
