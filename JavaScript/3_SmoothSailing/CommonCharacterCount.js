0c88b0b9038e6f2b93a2bd0cae0dde7b6c75fe60
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-05 15:59:40 -0500

    Add s1 characters to library

--- JavaScript/3_SmoothSailing/CommonCharacterCount.js
+++ JavaScript/3_SmoothSailing/CommonCharacterCount.js
@@ -1,3 +1,16 @@
 function commonCharacterCount(s1, s2) {
-
+    let lib={};
+    for (let chr in s1){
+        if (lib[s1[chr]] !== undefined){
+            lib[s1[chr]] += 1;
+        } else {
+            lib[s1[chr]] = 1;
+        }
+    }
+    console.log(lib);
 }
+
+let s1 = "aabcc"
+let s2 = "adcaa"
+
+commonCharacterCount(s1,s2);
