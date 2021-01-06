ac63ca6ff186c7d3447904efdc8ab98bf19e2f35
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-06 16:19:36 -0500

    Loop through half the array. Add left side and right side.

--- JavaScript/3_SmoothSailing/IsLucky.js
+++ JavaScript/3_SmoothSailing/IsLucky.js
@@ -1,3 +1,18 @@
-function isLucky(n) {
-
+function isLucky(n) { 
+    let digits=n.toString().split('');
+    let left=0;
+    let right=0;
+    for (let x=0;x<digits.length/2;x++){
+        left+=parseInt(digits[x]);
+        right+=parseInt(digits[digits.length-x-1]);
+    }
+    console.log(left);
+    console.log(right);
+    return true;
 }
+
+let n1=1230;
+let n2=23917;
+
+console.log(isLucky(n1));
+console.log(isLucky(n2));
