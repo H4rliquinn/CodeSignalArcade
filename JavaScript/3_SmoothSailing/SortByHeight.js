09feabe0ad2ce9158824c17bd545700bc4cbf71d
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-07 15:33:05 -0500

    Sort complete ignoring trees

--- JavaScript/3_SmoothSailing/SortByHeight.js
+++ JavaScript/3_SmoothSailing/SortByHeight.js
@@ -1,7 +1,28 @@
 function sortByHeight(a) {
-    for (let x=0;x<a.length;x++){
-        console.log(a[x]);
+    let min=1000;
+    let current=0;
+    let swap=-1;
+
+    while (current<a.length){
+        if (a[current]!==-1){
+            min=a[current];
+            for (let x=current+1;x<a.length;x++){
+                if (a[x]!==-1 && a[x]<min){
+                    min=a[x];
+                    swap=x;
+                }
+            }
+            if (swap>-1){
+                let temp=a[current];
+                a[current]=a[swap];
+                a[swap]=temp;
+                swap=-1;
+            }
+        }       
+        current++;
     }
+    return a;
+
 }
 
 let a = [-1, 150, 190, 170, -1, -1, 160, 180];
