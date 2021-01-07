5694c990918edcae306c64c0b1ef6295693d4767
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-07 15:11:10 -0500

    Iterate through the array to sort

--- JavaScript/3_SmoothSailing/SortByHeight.js
+++ JavaScript/3_SmoothSailing/SortByHeight.js
@@ -1,3 +1,8 @@
 function sortByHeight(a) {
-
+    for (let x=0;x<a.length;x++){
+        console.log(a[x]);
+    }
 }
+
+let a = [-1, 150, 190, 170, -1, -1, 160, 180];
+console.log(sortByHeight(a));
