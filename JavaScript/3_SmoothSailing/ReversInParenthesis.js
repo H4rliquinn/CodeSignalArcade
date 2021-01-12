705e4f45e0b513c531a751a7e44b3f7bee665c5c
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-12 16:19:20 -0500

    Added tests and loop

--- JavaScript/3_SmoothSailing/ReversInParenthesis.js
+++ JavaScript/3_SmoothSailing/ReversInParenthesis.js
@@ -1,3 +1,16 @@
 function reverseInParentheses(inputString) {
+    let start=-1;
+    let end=-1;
 
+    for (let x in inputString){
+        console.log(inputString[x]);
+    }
 }
+
+let is="(bar)";
+console.log(reverseInParentheses(is));
+// let is="f(bar)f";
+// console.log(reverseInParentheses(is));
+// let is="foo(bar(baz))blim";
+//foobazrabblim
+// console.log(reverseInParentheses(is2));
