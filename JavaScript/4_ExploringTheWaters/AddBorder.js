9358f2fce8210f02ec877ff4046ab807d8932856
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-18 17:53:04 -0500

    Top and bottom lines added

--- JavaScript/4_ExploringTheWaters/AddBorder.js
+++ JavaScript/4_ExploringTheWaters/AddBorder.js
@@ -1,3 +1,10 @@
 function addBorder(picture) {
-
+    let line="*".repeat(picture[0].length+2);
+    picture.unshift(line);
+    picture.push(line);
+    for(x in picture){console.log(picture[x]);}
 }
+
+let pic=["abc","def"];
+
+addBorder(pic);
