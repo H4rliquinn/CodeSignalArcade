247ea583d2271144e8f558b561c4e1c008d5a32f
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-18 17:57:31 -0500

    Added L/R border. Return picture. All tests passed. Function no longer pure.

--- JavaScript/4_ExploringTheWaters/AddBorder.js
+++ JavaScript/4_ExploringTheWaters/AddBorder.js
@@ -1,10 +1,12 @@
 function addBorder(picture) {
     let line="*".repeat(picture[0].length+2);
+    for(x in picture){picture[x]="*"+picture[x]+"*";}
     picture.unshift(line);
     picture.push(line);
-    for(x in picture){console.log(picture[x]);}
+    return picture
 }
 
+// for(x in picture){console.log(picture[x]);}
 let pic=["abc","def"];
 
 addBorder(pic);
