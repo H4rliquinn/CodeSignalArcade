a5f093e30605275c64ef6a3ed75ef26d454913d9
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-19 15:51:13 -0500

    Manually checks that arrays have same elements 8/10 tests passed. Instructions unclear about number of swaps.

--- JavaScript/4_ExploringTheWaters/AreSimilar.js
+++ JavaScript/4_ExploringTheWaters/AreSimilar.js
@@ -1,3 +1,28 @@
 function areSimilar(a, b) {
-
+    let library={};
+    for (let x in a){
+        if (a[x] in library){
+            library[a[x]]++;
+        } else {
+            library[a[x]]=1;
+        }
+    }
+    for (let y in b){
+        if (library[b[y]]==undefined || library[b[y]]==0){
+            return false;
+        } else {
+            library[b[y]]--;
+        }
+    }
+    return true;
 }
+
+let a=[1,2,3];
+let b=[1,2,3];
+console.log(areSimilar(a,b));
+a=[1,2,3];
+b=[2,1,3];
+console.log(areSimilar(a,b));
+a=[1,2,2];
+b=[1,2,1];
+console.log(areSimilar(a,b));
