3dc6c33efe242b0032fd88bc1a3060496764fcd8
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-21 14:30:05 -0500

    Pretty straightforward logic it seems. Concerned directions not clear.

--- JavaScript/4_ExploringTheWaters/ArrayChange.js
+++ JavaScript/4_ExploringTheWaters/ArrayChange.js
@@ -1,3 +1,13 @@
 function arrayChange(inputArray) {
+    let current=Number.NEGATIVE_INFINITY;
+    let moves=0;
 
+    for (let x in inputArray){
+        if (inputArray[x]<=current){
+            moves+=current-(inputArray[x]+1);
+            current=moves++;
+        }
+    }
+    return moves;
 }
+
