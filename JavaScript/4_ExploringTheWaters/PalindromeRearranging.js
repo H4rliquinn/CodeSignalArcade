16112dd71cdc570902e7941eb1feb943d4e2abaf
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-22 14:09:09 -0500

    Library of characters for rearrangement created tests added

--- JavaScript/4_ExploringTheWaters/PalindromeRearranging.js
+++ JavaScript/4_ExploringTheWaters/PalindromeRearranging.js
@@ -1,3 +1,18 @@
 function palindromeRearranging(inputString) {
-
+    let lib={};
+    for (let x in inputString){
+        if (lib[inputString[x]]==1){
+            lib[inputString[x]]=0;
+        } else {//if (lib[inputString[x]]==0 || lib[inputString[x]]==undefined)
+            lib[inputString[x]]=1;
+        }
+    }
+    return lib;
 }
+
+let i="aabb";
+console.log(palindromeRearranging(i));
+i="acabb";
+console.log(palindromeRearranging(i));
+i="aabbco";
+console.log(palindromeRearranging(i));
