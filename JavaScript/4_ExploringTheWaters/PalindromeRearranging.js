57b80630c3e82c924b651cb2f267f7471343c53a
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-25 19:49:09 -0500

    Added scan through lib All tests passed

--- JavaScript/4_ExploringTheWaters/PalindromeRearranging.js
+++ JavaScript/4_ExploringTheWaters/PalindromeRearranging.js
@@ -1,5 +1,8 @@
 function palindromeRearranging(inputString) {
     let lib={};
+    let maxCenter=inputString.length%2==0?0:1;
+    let center=0;
+
     for (let x in inputString){
         if (lib[inputString[x]]==1){
             lib[inputString[x]]=0;
@@ -7,10 +10,22 @@ function palindromeRearranging(inputString) {
             lib[inputString[x]]=1;
         }
     }
-    return lib;
+
+    for (let y in lib){
+        if (lib[y]>0){
+            if (center<maxCenter){
+                center++;
+            } else if (center>=maxCenter){
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 let i="aabb";
+console.log(palindromeRearranging(i));
+i="acbb";
 console.log(palindromeRearranging(i));
 i="acabb";
 console.log(palindromeRearranging(i));
