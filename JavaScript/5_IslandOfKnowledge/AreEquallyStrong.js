66b14d59861091037a969462eb69b8c52a683c10
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-26 20:30:50 -0500

    All tests passed

--- JavaScript/5_IslandOfKnowledge/AreEquallyStrong.js
+++ JavaScript/5_IslandOfKnowledge/AreEquallyStrong.js
@@ -1,3 +1,21 @@
 function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
-
+    let yourStrong=Math.max(yourLeft,yourRight);
+    let yourWeak=Math.min(yourLeft,yourRight);
+    let friendStrong=Math.max(friendsLeft,friendsRight);
+    let friendWeak=Math.min(friendsLeft,friendsRight);
+    
+    return (yourStrong==friendStrong && yourWeak==friendWeak);
 }
+
+let yl=10,yr=15,fl=15,fr=10;
+console.log(areEquallyStrong(yl,yr,fl,fr));
+yl=15;
+yr=10;
+fl=15;
+fr=10;
+console.log(areEquallyStrong(yl,yr,fl,fr));
+yl=15;
+yr=10;
+fl=15;
+fr=9;
+console.log(areEquallyStrong(yl,yr,fl,fr));
