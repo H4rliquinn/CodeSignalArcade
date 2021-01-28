afd3e0f602c1270975e44b0ecf1c1f3e4374cb54
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-28 13:14:18 -0500

    Calculated each distance saved to max

--- JavaScript/5_IslandOfKnowledge/arrayMaximalAdjacentDifference.js
+++ JavaScript/5_IslandOfKnowledge/arrayMaximalAdjacentDifference.js
@@ -1,3 +1,12 @@
 function arrayMaximalAdjacentDifference(inputArray) {
-
+    let last,current;
+    let max=-1;
+    for (let x in inputArray){
+        if (last!=undefined){
+            current=Math.abs(last-inputArray[x]);
+            if (max<current) max=current;
+        }
+        last=inputArray[x]
+    }
 }
+
