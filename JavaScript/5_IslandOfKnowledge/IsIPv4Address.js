8ba241670c9ef6629420b1d2910057cfe602b3cf
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-29 14:25:43 -0500

    Added RegEx for letters. Need to test multi 0

--- JavaScript/5_IslandOfKnowledge/IsIPv4Address.js
+++ JavaScript/5_IslandOfKnowledge/IsIPv4Address.js
@@ -1,18 +1,25 @@
 function isIPv4Address(inputString) {
     let nums=inputString.split(".");
     if (nums.length!=4)return false;
+
     for (let x in nums){
         let test=parseInt(nums[x]);
-        if (test>255 || test<0 ||  !Number.isInteger(test)) return false;
+        if (nums[x].match(/\D/)){
+            console.log("True",nums[x].match(/\D/),"*");
+            return false;
+        }
+        if (test>255 || test<0 ||  !Number.isInteger(test)|| test.match) return false;
     }
     return true;
 }
 
-let is="172.16.254.1";
+// let is="172.16.254.1";
+// console.log(isIPv4Address(is));
+is="172.16.254.1a";
 console.log(isIPv4Address(is));
-is="172.316.254.1";
-console.log(isIPv4Address(is));
-is="172.e.254.1";
-console.log(isIPv4Address(is));
-is=".16.254.1"
-console.log(isIPv4Address(is));
+// is="172.316.254.1";
+// console.log(isIPv4Address(is));
+// is="172.e.254.1";
+// console.log(isIPv4Address(is));
+// is=".16.254.1"
+// console.log(isIPv4Address(is));
