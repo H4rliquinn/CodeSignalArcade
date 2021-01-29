72167a820fa15385ed26c65bda1aee85f88254a8
Jason Sonnichsen <Harliquinn@gmail.com>
2021-01-29 14:01:08 -0500

    Tests set up. Need to deal with combinations like 1a

--- JavaScript/5_IslandOfKnowledge/IsIPv4Address.js
+++ JavaScript/5_IslandOfKnowledge/IsIPv4Address.js
@@ -1,7 +1,18 @@
 function isIPv4Address(inputString) {
     let nums=inputString.split(".");
-    return nums;
+    if (nums.length!=4)return false;
+    for (let x in nums){
+        let test=parseInt(nums[x]);
+        if (test>255 || test<0 ||  !Number.isInteger(test)) return false;
+    }
+    return true;
 }
 
 let is="172.16.254.1";
+console.log(isIPv4Address(is));
+is="172.316.254.1";
+console.log(isIPv4Address(is));
+is="172.e.254.1";
+console.log(isIPv4Address(is));
+is=".16.254.1"
 console.log(isIPv4Address(is));
