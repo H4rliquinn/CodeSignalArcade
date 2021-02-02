963d99c5ba6f289b0af874a0b458f0ddd7daddc0
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-02 13:21:54 -0500

    Loop through possible answers

--- JavaScript/5_IslandOfKnowledge/AvoidObstacles.js
+++ JavaScript/5_IslandOfKnowledge/AvoidObstacles.js
@@ -1,3 +1,9 @@
 function avoidObstacles(inputArray) {
-
+ let current=0;
+ for (let x=0;x<Math.max(...inputArray);x++){
+     console.log(x);
+ }
 }
+
+let ia=[5,3,6,7,9];
+console.log(avoidObstacles(ia));
