8ddc9ae67026ba0338bd14313dc9f96478d14f5f
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-03 19:26:27 -0500

    Pixels Found

--- JavaScript/5_IslandOfKnowledge/BoxBlur.js
+++ JavaScript/5_IslandOfKnowledge/BoxBlur.js
@@ -1,3 +1,25 @@
 function boxBlur(image) {
-
+    let blurred=[];
+    //Find pixels Y axis
+    for (let y=1;y<image.length-1;y++){
+        //Find pixels X axis
+        for (let x=1;x<image[y].length-1;x++){
+            //Send pixel values to helper function
+            console.log(image[y][x]);
+        }
+    }
+    //write to output
 }
+
+let i=[[1,1,1],
+        [1,7,1],
+        [1,1,1]];
+
+console.log(boxBlur(i));
+
+i=[[7, 4, 0, 1], 
+[5, 6, 2, 2], 
+[6, 10, 7, 8], 
+[1, 4, 2, 0]];
+
+console.log(boxBlur(i));
