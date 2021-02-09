9c9207710eae37b261b3ea1ec31026d75c436a9f
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-09 14:52:51 -0500

    Adjusted to assume rectangular matrix. Alll tests passed

--- JavaScript/5_IslandOfKnowledge/Minesweeper.js
+++ JavaScript/5_IslandOfKnowledge/Minesweeper.js
@@ -1,24 +1,22 @@
 function minesweeper(matrix) {
- let retMtx=new Array(matrix.length).fill(0).map(()=>new Array(matrix.length).fill(0));
- let len=matrix.length;
- for (let y=0;y<len;y++){
-     for (let x=0;x<len;x++){
+ //Assume rectangular matrix
+ let retMtx=new Array(matrix.length).fill(0).map(()=>new Array(matrix[0].length).fill(0));
+
+ for (let y=0;y<matrix.length;y++){
+     for (let x=0;x<matrix[y].length;x++){
          if (matrix[y][x]==true){
             //Determine where to add to count based on bomb position
             let countY=[0];
             let countX=[0];
             if (y>0)countY.push(-1);
-            if (y<len-1)countY.push(1);
+            if (y<matrix.length-1)countY.push(1);
             if (x>0)countX.push(-1);
-            if (x<len-1)countX.push(1);
-            // console.log("adds",y,x,countY,countX);
+            if (x<matrix[y].length-1)countX.push(1);
 
             //Add to count around current bomb
             for (let a in countY){
                 for (let b in countX){
                     if (!(countY[a]==0 && countX[b]==0)) retMtx[y+countY[a]][x+countX[b]]++;
-                    // console.log("Added:",b,a,retMtx);
-                    // console.log("A&B",countY[a],countX[b],retMtx[y+countY[a]][x+countX[b]]);
                 }
             }
          }
