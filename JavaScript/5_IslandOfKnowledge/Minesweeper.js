5c2fbaf0575ec853e5df7faa59468e4ddc231c29
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-09 14:48:12 -0500

    Added counts for bombs Tests have not square matrixes.

--- JavaScript/5_IslandOfKnowledge/Minesweeper.js
+++ JavaScript/5_IslandOfKnowledge/Minesweeper.js
@@ -4,23 +4,37 @@ function minesweeper(matrix) {
  for (let y=0;y<len;y++){
      for (let x=0;x<len;x++){
          if (matrix[y][x]==true){
+            //Determine where to add to count based on bomb position
             let countY=[0];
             let countX=[0];
             if (y>0)countY.push(-1);
             if (y<len-1)countY.push(1);
             if (x>0)countX.push(-1);
             if (x<len-1)countX.push(1);
-            console.log(x,y,countY,countX);
+            // console.log("adds",y,x,countY,countX);
 
+            //Add to count around current bomb
+            for (let a in countY){
+                for (let b in countX){
+                    if (!(countY[a]==0 && countX[b]==0)) retMtx[y+countY[a]][x+countX[b]]++;
+                    // console.log("Added:",b,a,retMtx);
+                    // console.log("A&B",countY[a],countX[b],retMtx[y+countY[a]][x+countX[b]]);
+                }
+            }
          }
      }
  }
-
  return retMtx;
 }
 
 let matrix = [[true, false, false],
 [false, true, false],
 [false, false, false]];
+
+console.log(minesweeper(matrix));
+
+matrix = [[true, false, false],
+        [false, true, false],
+        [false, true, true]];
 
 console.log(minesweeper(matrix));
