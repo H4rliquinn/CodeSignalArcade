4aa4282c1d6a01837cdd40ac0e289d45ba9d2964
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-11 16:17:23 -0500

    Added RegEx and added tests

--- JavaScript/6_RainsOfReason/VariableName.js
+++ JavaScript/6_RainsOfReason/VariableName.js
@@ -1,3 +1,14 @@
 function variableName(name) {
-
+    if(/^\D\w*/){
+        return true;
+    } else {
+        return false;
+    }
 }
+
+let nm="var_1__Int";
+variableName(nm);
+nm="qq-q";
+variableName(nm);
+nm="2w2";
+variableName(nm);
