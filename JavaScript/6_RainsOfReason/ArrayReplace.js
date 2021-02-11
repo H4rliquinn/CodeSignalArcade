3707983dccf9730718b8f7d56cae58b83f5c2037
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-11 16:00:30 -0500

    Simple replacement. All tests passed

--- JavaScript/6_RainsOfReason/ArrayReplace.js
+++ JavaScript/6_RainsOfReason/ArrayReplace.js
@@ -1,3 +1,8 @@
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
-
+    for (let x=0;x<inputArray.length;x++){
+        if (inputArray[x]==elemToReplace){
+            inputArray[x]=substitutionElem;
+        }
+    }
+    return inputArray;
 }
