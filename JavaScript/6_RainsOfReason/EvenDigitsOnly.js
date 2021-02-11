ccfdd567a4e2e74b6f0a0c51d7dc82f03297e438
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-11 16:12:12 -0500

    All tests passed

--- JavaScript/6_RainsOfReason/EvenDigitsOnly.js
+++ JavaScript/6_RainsOfReason/EvenDigitsOnly.js
@@ -1,3 +1,15 @@
 function evenDigitsOnly(n) {
-
+    let arr=n.toString().split("");
+    for (let x in arr){
+        if (arr[x]%2!=0){
+            return false;   
+        }
+    }
+    return true;
 }
+
+let num=248622;
+console.log(evenDigitsOnly(num));
+
+num=4672;
+console.log(evenDigitsOnly(num));
