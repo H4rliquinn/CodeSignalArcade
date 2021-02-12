32d1a514084aa75560d5cce730f40e89e0b85962
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-12 15:09:07 -0500

    Loop through string add to return value

--- JavaScript/6_RainsOfReason/AlphabeticShift.js
+++ JavaScript/6_RainsOfReason/AlphabeticShift.js
@@ -1,3 +1,12 @@
 function alphabeticShift(inputString) {
-
+    let retString="";
+    for (let x in inputString){
+        // console.log(inputString[x]);
+        retString+=inputString[x];
+    }
+    return retString;
 }
+
+let is="crazy";
+
+console.log(alphabeticShift(is));
