6f3ae42fff1c33eca8d34b7910b1222171825497
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-17 14:48:41 -0500

    Helper function to determine color created

--- JavaScript/6_RainsOfReason/ChessBoardCellColor.js
+++ JavaScript/6_RainsOfReason/ChessBoardCellColor.js
@@ -1,7 +1,31 @@
+function checkColor(num1,num2){
+    if (parseInt(num1)%2==0){
+        //Even
+        if (num2%2==0){
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        //Odd
+        if (num2%2==0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
 function chessBoardCellColor(cell1, cell2) {
-    let [c1r,c1c]=cell1.split('');
-    console.log(c1r,c1c);
-    return true;
+    let [c1Col,c1Row]=cell1.split('');
+    let [c2Col,c2Row]=cell2.split('');
+    let lib={'A':1,'B':2,'C':3,'D':4,'E':5,'F':6};
+
+    // console.log(c1Col,c1Row,c2Col,c2Row);
+
+    console.log(checkColor(parseInt(c1Row),lib[c1Col]));
+    
+    
 }
 
 let c1='A1';
