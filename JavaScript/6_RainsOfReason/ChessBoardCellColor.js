d575c23d05351dacf435f05a2a5dbc04bfc97c42
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-17 14:35:15 -0500

    Javascript has multiple inheritance? Cool.

---
+++ JavaScript/6_RainsOfReason/ChessBoardCellColor.js
@@ -0,0 +1,10 @@
+function chessBoardCellColor(cell1, cell2) {
+    let [c1r,c1c]=cell1.split('');
+    console.log(c1r,c1c);
+    return true;
+}
+
+let c1='A1';
+let c2='C3';
+
+console.log(chessBoardCellColor(c1,c2));
