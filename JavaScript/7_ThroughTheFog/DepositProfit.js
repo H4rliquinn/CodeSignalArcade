15b762057f433af36b3664d1c51914b7527d8c01
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-19 15:00:53 -0500

    All tests passed

--- JavaScript/7_ThroughTheFog/DepositProfit.js
+++ JavaScript/7_ThroughTheFog/DepositProfit.js
@@ -1,3 +1,16 @@
 function depositProfit(deposit, rate, threshold) {
-
+    let percent=rate/100+1;
+    let current=deposit;
+    let yearCount=0;
+    while (current<threshold){
+        yearCount++;
+        current*=percent;
+    }
+    return yearCount;
 }
+
+let dep=100;
+let r=20;
+let target=170;
+
+console.log(depositProfit(dep,r,target));
