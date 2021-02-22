ced01557ecc94f3f03b2d27706f41dc2a7e59005
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-22 18:36:12 -0500

    Use reduce to find the total for each item.

--- JavaScript/7_ThroughTheFog/AbsoluteValuesSumMinimization.js
+++ JavaScript/7_ThroughTheFog/AbsoluteValuesSumMinimization.js
@@ -1,5 +1,10 @@
 function absoluteValuesSumMinimization(a) {
 
+    let daMin=a.reduce((tot,curr)=>{ 
+        console.log(a[0],curr);
+        return tot+Math.abs(a[0]-curr)
+    },0);
+    console.log(daMin);
 }
 
 let arr=[2,4,7];
