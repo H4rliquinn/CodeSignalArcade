f6c8f62ce08d6c48e46eda4526bbe9421a3a967d
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-22 18:43:45 -0500

    Return smallest value.

--- JavaScript/7_ThroughTheFog/AbsoluteValuesSumMinimization.js
+++ JavaScript/7_ThroughTheFog/AbsoluteValuesSumMinimization.js
@@ -2,12 +2,10 @@ function absoluteValuesSumMinimization(a) {
     let retVal=[];
     for (let y=0;y<a.length;y++){
         retVal.push(a.reduce((tot,curr)=>{ 
-            console.log(a[y],curr);
             return tot+Math.abs(a[y]-curr)
         },0));
     }
-
-    return retVal
+    return a[retVal.indexOf(Math.min(...retVal))];
 }
 
 let arr=[2,4,7];
