3a89f69f974bc655e95385a00871141c2636a034
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-25 12:49:02 -0500

    For each pair of strings check letters

--- JavaScript/7_ThroughTheFog/StringsRearrangement.js
+++ JavaScript/7_ThroughTheFog/StringsRearrangement.js
@@ -1,3 +1,18 @@
-function stringsRearrangement(inputArray) {
-
+function oneApart(s1,s2){
+    //Check if strings are one letter appart
+    let count=0;
+    for (let x in s1){
+        console.log(s1[x],s2[x]);
+    }
 }
+
+function stringsRearrangement(inputArray) {
+    for (let x=1;x<inputArray.length;x++){
+        oneApart(inputArray[x],inputArray[x-1])
+    }
+}
+
+
+let ia=["aba", "bbb", "bab"];
+
+console.log(stringsRearrangement(ia));
