bc63330c9ff6aba612131103b02291ea32b2044c
Jason Sonnichsen <Harliquinn@gmail.com>
2021-02-25 12:49:59 -0500

    One Apart return boolean

--- JavaScript/7_ThroughTheFog/StringsRearrangement.js
+++ JavaScript/7_ThroughTheFog/StringsRearrangement.js
@@ -2,13 +2,22 @@ function oneApart(s1,s2){
     //Check if strings are one letter appart
     let count=0;
     for (let x in s1){
-        console.log(s1[x],s2[x]);
+        // console.log(s1[x],s2[x]);
+        if (s1[x]!=s2[x]){
+            if (count<2){
+                count++;
+            } else {
+                return false;
+            }
+        }
     }
+    return true;
 }
 
 function stringsRearrangement(inputArray) {
     for (let x=1;x<inputArray.length;x++){
-        oneApart(inputArray[x],inputArray[x-1])
+        console.log(oneApart(inputArray[x],inputArray[x-1]));
+        
     }
 }
 
