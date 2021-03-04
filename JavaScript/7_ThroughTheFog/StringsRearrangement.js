01b5d5c5c88878f916904acb21b713f2e0700f24
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-03 22:01:26 -0500

    Getting close, doubling for some reason

--- JavaScript/7_ThroughTheFog/StringsRearrangement.js
+++ JavaScript/7_ThroughTheFog/StringsRearrangement.js
@@ -15,17 +15,19 @@ function oneApart(s1,s2){
 }
 
 function permutationArray(ia,curr,cache){
-    if (ia.lengh==1) {
+    if (ia.length==1) {
+        console.log("1 Left",ia,curr,cache);
         curr.push(ia[0]);
         cache.push(curr);
         return cache;
     }
     
     for (let i=0;i<ia.length;i++){
+        console.log("Loop",ia,curr,cache);
         curr.push(ia[i]);
         let newArray=[...ia];
         newArray.splice(i,1);
-        permutationArray(newArray,curr,cache)
+        cache=permutationArray(newArray,curr,cache)
     }
     return cache;
 }
@@ -42,6 +44,6 @@ function stringsRearrangement(inputArray) {
 
 
 let ia=["aba", "bbb", "bab"];
-ia=["aba", "bbb"];
+ia=["1", "2"];
 
 console.log(stringsRearrangement(ia));
