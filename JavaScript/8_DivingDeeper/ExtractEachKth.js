f8c636c03e5efd586d7d4a5c2f02803b7aff6d32
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-09 15:15:11 -0500

    Returning last index to remove

--- JavaScript/8_DivingDeeper/ExtractEachKth.js
+++ JavaScript/8_DivingDeeper/ExtractEachKth.js
@@ -1,3 +1,9 @@
 function extractEachKth(inputArray, k) {
-
+    let currIndex=parseInt(inputArray.length/k)*k;
+    
 }
+
+let ia=[1,2,3,4,5,6,7,8,9,10];
+let k=3;
+
+console.log(extractEachKth(ia,k));
