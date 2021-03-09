23fb80cbc8212ed1b326293777806718d3c86d96
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-09 15:24:51 -0500

    Removed elements back to front All tests passed

--- JavaScript/8_DivingDeeper/ExtractEachKth.js
+++ JavaScript/8_DivingDeeper/ExtractEachKth.js
@@ -1,6 +1,12 @@
 function extractEachKth(inputArray, k) {
     let currIndex=parseInt(inputArray.length/k)*k;
     
+    while (currIndex>0){
+        inputArray.splice(currIndex-1,1);
+        currIndex-=k;
+    }
+    
+    return inputArray;
 }
 
 let ia=[1,2,3,4,5,6,7,8,9,10];
