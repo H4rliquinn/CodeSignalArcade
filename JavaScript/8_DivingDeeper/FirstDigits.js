cbf1f95501214a63a2531c28089ab611033f8751
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-10 11:26:53 -0500

    Ran through characters. Returned first number All tests passed

--- JavaScript/8_DivingDeeper/FirstDigits.js
+++ JavaScript/8_DivingDeeper/FirstDigits.js
@@ -1,3 +1,15 @@
 function firstDigit(inputString) {
-
+    for (let x in inputString){
+        if (inputString[x].match(/\d/)){
+            return inputString[x];
+        }
+    }
+    return false;
 }
+
+let is="var_1__Int";
+console.log(firstDigit(is));
+is="q2q-q";
+console.log(firstDigit(is));
+is="0ss"
+console.log(firstDigit(is));
