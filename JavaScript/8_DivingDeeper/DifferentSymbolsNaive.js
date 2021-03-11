c8e11db9df517acda53d492c7e90ad1850d0e4c8
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-11 16:55:31 -0500

    All tests pased

--- JavaScript/8_DivingDeeper/DifferentSymbolsNaive.js
+++ JavaScript/8_DivingDeeper/DifferentSymbolsNaive.js
@@ -1,3 +1,16 @@
 function differentSymbolsNaive(s) {
+    let lib={};
+    let count=0;
 
+    s.split();
+    for (let x in s){
+        if (lib[s[x]]==undefined){
+            lib[s[x]]=0;
+            count++;
+        }
+    }
+    return count;
 }
+
+let s="cabca";
+console.log(differentSymbolsNaive(s));
