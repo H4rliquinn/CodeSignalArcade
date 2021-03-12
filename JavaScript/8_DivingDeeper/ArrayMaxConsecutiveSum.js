28e209348e6dcda2a510398b774db152058c5015
Jason Sonnichsen <Harliquinn@gmail.com>
2021-03-12 16:50:15 -0500

    Track top value. All tests passed.

--- JavaScript/8_DivingDeeper/ArrayMaxConsecutiveSum.js
+++ JavaScript/8_DivingDeeper/ArrayMaxConsecutiveSum.js
@@ -1,12 +1,16 @@
 function arrayMaxConsecutiveSum(inputArray, k) {
     let sum=0;
+    let max=0;
     for (let x=0;x<inputArray.length;x++){
         sum+=inputArray[x];
         if (x>k-1){
             sum-=inputArray[x-k];
         }
+        if (sum>max){
+            max=sum;
+        }
     }
-    return sum;
+    return max;
 }
 
 let ia=[2,3,5,1,6];
