f06c475109ce7af02cf810ce536cfda5ea5205dc
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-14 14:19:33 -0400

    Simple algorithm. All tests passed.

--- JavaScript/9_DarkWilderness/GrowingPlant.js
+++ JavaScript/9_DarkWilderness/GrowingPlant.js
@@ -1,3 +1,9 @@
 function growingPlant(upSpeed, downSpeed, desiredHeight) {
-
+    let height=upSpeed;
+    let days=1;
+    while (height<desiredHeight){
+        height+=upSpeed-downSpeed;
+        days++;
+    }
+    return days;
 }
