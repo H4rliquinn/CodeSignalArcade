00fd9338aac343a9e65bc8f9996ca5df52aef2c9
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-14 14:31:02 -0400

    Simplified Knapsack Problem. All tests passed

--- JavaScript/9_DarkWilderness/KnapsackLight.js
+++ JavaScript/9_DarkWilderness/KnapsackLight.js
@@ -1,3 +1,14 @@
 function knapsackLight(value1, weight1, value2, weight2, maxW) {
-
+    if (weight1+weight2<=maxW){
+        return value1+value2;
+    } else {
+        retVal=0;
+        if (weight1<=maxW){
+            retVal=value1;
+        }
+        if (weight2<=maxW && value2>retVal){
+                retVal=value2;
+        }
+        return retVal;
+    }
 }
