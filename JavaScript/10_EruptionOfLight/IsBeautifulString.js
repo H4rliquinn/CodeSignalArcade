d4422ad95057a357a96d27278aa3d7fe0673939c
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-16 14:01:42 -0400

    Verifying there's an "a" Setting count variables

--- JavaScript/10_EruptionOfLight/IsBeautifulString.js
+++ JavaScript/10_EruptionOfLight/IsBeautifulString.js
@@ -1,3 +1,18 @@
 function isBeautifulString(inputString) {
+    let chars=inputString.split("").sort();
+    if (chars[0]!="a") return false;
+    let currChar=chars[0].charCodeAt(0);
+    let currCount=0;
+    let lastCount=0;
+    // for (let x in chars){
 
+    // }
+    return currChar;
 }
+
+let s="bbbaacdafe";
+console.log(isBeautifulString(s));
+s="aabbb";
+console.log(isBeautifulString(s));
+s="bbc";
+console.log(isBeautifulString(s));
