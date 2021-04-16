43b8ce63a9e5d6ff98193f1c25cb5a5b1a0b406e
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-16 14:15:06 -0400

    Close, comparison is off

--- JavaScript/10_EruptionOfLight/IsBeautifulString.js
+++ JavaScript/10_EruptionOfLight/IsBeautifulString.js
@@ -4,15 +4,24 @@ function isBeautifulString(inputString) {
     let currChar=chars[0].charCodeAt(0);
     let currCount=0;
     let lastCount=0;
-    // for (let x in chars){
-
-    // }
-    return currChar;
+    for (let x in chars){
+        console.log(currChar,currCount,lastCount,chars[x].charCodeAt(0));
+        if (chars[x].charCodeAt(0)==currChar){
+            currCount++;
+        } else if (currChar!=97 && currCount>lastCount){
+            return false;
+        } else {
+            currChar=chars[x].charCodeAt(0)
+            lastCount=currCount;
+            currCount=0;
+        }
+    }
+    return true;
 }
 
 let s="bbbaacdafe";
-console.log(isBeautifulString(s));
+// console.log(isBeautifulString(s));
 s="aabbb";
 console.log(isBeautifulString(s));
 s="bbc";
-console.log(isBeautifulString(s));
+// console.log(isBeautifulString(s));
