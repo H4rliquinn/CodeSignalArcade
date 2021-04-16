750b3f32ca6a7036fe0eae6462c6f05fe00b40b3
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-16 13:47:56 -0400

    All tests passed

--- JavaScript/9_DarkWilderness/BishopAndPawn.js
+++ JavaScript/9_DarkWilderness/BishopAndPawn.js
@@ -1,8 +1,10 @@
 function bishopAndPawn(bishop, pawn) {
-    // return Math.abs(bishop[1]-pawn[1]);
-    return (Math.abs(bishop.charCodeAt(0)-pawn.charCodeAt(0)));
+    return (Math.abs(bishop.charCodeAt(0)-pawn.charCodeAt(0))==Math.abs(bishop[1]-pawn[1]));
 }
 
 let bish="a1";
 let paw="c3";
+console.log(bishopAndPawn(bish,paw));
+bish="h1";
+paw="h3";
 console.log(bishopAndPawn(bish,paw));
