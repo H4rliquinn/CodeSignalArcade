128e12ff447d7942cfc140c257d0b176e1269562
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-23 14:19:59 -0400

    Solved base problem, testing

--- JavaScript/10_EruptionOfLight/BuildPalindrome.js
+++ JavaScript/10_EruptionOfLight/BuildPalindrome.js
@@ -1,16 +1,28 @@
 function buildPalindrome(st) {
     let currAdd=st.length-2;
-    let currString=st;
     let pal=false;
 
     while (pal==false){
-        for(let x=0;x<Math.floor(st.length/2);x++){
-            console.log(st[x],st[st.length-1-x]);
-        }
         pal=true;
+        for(let x=0;x<Math.floor(st.length/2);x++){
+            if (st[x]!=st[st.length-1-x]){
+                // console.log(st[x],st[st.length-1-x]);
+                pal=false;
+                break;
+            }
+        }
+        if (pal==false){
+            st=st+st[currAdd];
+            currAdd--;
+            // console.log(st);
+        }
     }
+    return st;
 }
 
 let a="abcdc";
-
+console.log(buildPalindrome(a));
+a="abcdc";
+console.log(buildPalindrome(a));
+a="abcdc";
 console.log(buildPalindrome(a));
