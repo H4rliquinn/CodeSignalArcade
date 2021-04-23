37380fd6e3b58968a851250cd37a00c7f0d53e2f
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-23 14:11:49 -0400

    Comparing outside-in

--- JavaScript/10_EruptionOfLight/BuildPalindrome.js
+++ JavaScript/10_EruptionOfLight/BuildPalindrome.js
@@ -1,5 +1,14 @@
 function buildPalindrome(st) {
+    let currAdd=st.length-2;
+    let currString=st;
+    let pal=false;
 
+    while (pal==false){
+        for(let x=0;x<Math.floor(st.length/2);x++){
+            console.log(st[x],st[st.length-1-x]);
+        }
+        pal=true;
+    }
 }
 
 let a="abcdc";
