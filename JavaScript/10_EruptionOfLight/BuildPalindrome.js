79ea2a677a23d81dc001c08149524b8ec6734a8c
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-29 18:14:51 -0400

    Found limits

--- JavaScript/10_EruptionOfLight/BuildPalindrome.js
+++ JavaScript/10_EruptionOfLight/BuildPalindrome.js
@@ -1,6 +1,26 @@
 function buildPalindrome(st) {
     //Find Existing Middles
-    //Check if middle goes to end
+    let y,z,flag;
+    for (let x=1;x<st.length-1;x++){
+        console.log(st[x-1],st[x+1]);
+        if (st[x-1]==st[x+1]){
+            //Found a middle
+            //Check if middle goes to end
+            y=x-1;
+            z=x+1;
+            flag=true;
+            while (y!=0 && z!=st.length-1 && flag==true){
+                y--;
+                z++
+                flag=st[y]==st[z];
+                // console.log(st[y],st[z],flag);
+            }
+            if(flag){
+                break;
+            }
+        }
+    }
+    console.log(y,z,flag);
     //Add missing characters
 
     // let currAdd=st.length-2;
