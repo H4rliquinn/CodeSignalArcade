bf084c1a0dd8d8d009d9b0ebfcf21ae6ab877137
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-29 17:54:28 -0400

    New Plan

--- JavaScript/10_EruptionOfLight/BuildPalindrome.js
+++ JavaScript/10_EruptionOfLight/BuildPalindrome.js
@@ -1,25 +1,28 @@
 function buildPalindrome(st) {
-    let currAdd=st.length-2;
-    let pal=false;
+    //Find Existing Middles
+    //Check if middle goes to end
+    //Add missing characters
 
-    while (pal==false){
-        pal=true;
-        for(let x=0;x<Math.floor(st.length/2);x++){
-            if (st[x]!=st[st.length-1-x]){
-                // console.log(st[x],st[st.length-1-x]);
-                pal=false;
-                break;
-            }
-        }
-        if (pal==false){
-            if (st[currAdd+1]!=st[currAdd-1]){
-                st=st+st[currAdd];
-            }
-            currAdd--;
-            console.log(st,currAdd);
-        }
-    }
-    return st;
+    // let currAdd=st.length-2;
+    // let pal=false;
+    // while (pal==false){
+    //     pal=true;
+    //     for(let x=0;x<Math.floor(st.length/2);x++){
+    //         if (st[x]!=st[st.length-1-x]){
+    //             // console.log(st[x],st[st.length-1-x]);
+    //             pal=false;
+    //             break;
+    //         }
+    //     }
+    //     if (pal==false){
+    //         if (st[currAdd+1]!=st[currAdd-1]){
+    //             st=st+st[currAdd];
+    //         }
+    //         currAdd--;
+    //         console.log(st,currAdd);
+    //     }
+    // }
+    // return st;
 }
 
 let a="abcdcb";
