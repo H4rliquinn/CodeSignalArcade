6e7c59d835fcddaebfa0014dba5e3e560c62a41b
Jason Sonnichsen <Harliquinn@gmail.com>
2021-04-30 18:08:04 -0400

    Add letters

--- JavaScript/10_EruptionOfLight/BuildPalindrome.js
+++ JavaScript/10_EruptionOfLight/BuildPalindrome.js
@@ -13,38 +13,22 @@ function buildPalindrome(st) {
                 y--;
                 z++
                 flag=st[y]==st[z];
-                // console.log(st[y],st[z],flag);
+                console.log(st[y],st[z],flag);
             }
             if(flag){
                 break;
             }
         }
     }
-    console.log(y,z,flag);
+    // console.log(y,z,flag);
     //Add missing characters
-
-    // let currAdd=st.length-2;
-    // let pal=false;
-    // while (pal==false){
-    //     pal=true;
-    //     for(let x=0;x<Math.floor(st.length/2);x++){
-    //         if (st[x]!=st[st.length-1-x]){
-    //             // console.log(st[x],st[st.length-1-x]);
-    //             pal=false;
-    //             break;
-    //         }
-    //     }
-    //     if (pal==false){
-    //         if (st[currAdd+1]!=st[currAdd-1]){
-    //             st=st+st[currAdd];
-    //         }
-    //         currAdd--;
-    //         console.log(st,currAdd);
-    //     }
-    // }
-    // return st;
+    while (y>0){
+        y--;
+        st+=st[y];
+    }
+    return st;
 }
 
-let a="abcdcb";
+let a="ababab";
 
 console.log(buildPalindrome(a));
