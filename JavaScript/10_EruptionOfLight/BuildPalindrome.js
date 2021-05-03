a940d3f3de3e83e68b402a706db990c14506ffc5
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-03 13:56:02 -0400

    Fixed for no middle found

--- JavaScript/10_EruptionOfLight/BuildPalindrome.js
+++ JavaScript/10_EruptionOfLight/BuildPalindrome.js
@@ -1,25 +1,50 @@
 function buildPalindrome(st) {
     //Find Existing Middles
-    let y,z,flag;
-    for (let x=1;x<st.length-1;x++){
+    let y=0,z,flag;
+   
         console.log(st[x-1],st[x+1]);
-        if (st[x-1]==st[x+1]){
-            //Found a middle
-            //Check if middle goes to end
-            y=x-1;
-            z=x+1;
-            flag=true;
-            while (y!=-1 && z!=st.length-1 && flag==true){
-                y--;
-                z++
-                flag=st[y]==st[z];
-                console.log(st[y],st[z],flag);
-            }
-            if(flag){
-                break;
+        if (st.length%2==1){
+            for (let x=1;x<st.length-1;x++){
+                if (st[x-1]==st[x+1]){
+                    //Found a middle
+                    //Check if middle goes to end
+                    y=x-1;
+                    z=x+1;
+                    flag=true;
+                    while (y!=-1 && z!=st.length-1 && flag==true){
+                        y--;
+                        z++
+                        flag=st[y]==st[z];
+                        console.log(st[y],st[z],flag);
+                    }
+                    if(flag){
+                        break;
+                    }
+                }       
+            }    
+        } else {
+            for (let x=1;x<st.length-1;x++){
+                if (st[x-1]==st[x+1]){
+                    //Found a middle
+                    //Check if middle goes to end
+                    y=x-1;
+                    z=x+1;
+                    flag=true;
+                    while (y!=-1 && z!=st.length-1 && flag==true){
+                        y--;
+                        z++
+                        flag=st[y]==st[z];
+                        console.log(st[y],st[z],flag);
+                    }
+                    if(flag){
+                        break;
+                    }
+                } 
             }
         }
-    }
+
+
+    if (y==0) y=st.length-1;
     // console.log(y,z,flag);
     //Add missing characters
     while (y>0){
@@ -30,5 +55,5 @@ function buildPalindrome(st) {
 }
 
 let a="ababab";
-
+// a="abc";
 console.log(buildPalindrome(a));
