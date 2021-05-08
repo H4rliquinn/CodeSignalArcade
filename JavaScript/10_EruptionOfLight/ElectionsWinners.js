affa84918cb779e83dde297aab02afbe4e4a8a7d
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-07 20:32:09 -0400

    Check for those still in the running

--- JavaScript/10_EruptionOfLight/ElectionsWinners.js
+++ JavaScript/10_EruptionOfLight/ElectionsWinners.js
@@ -1,5 +1,12 @@
 function electionsWinners(votes, k) {
-
+    let leader=Math.max(...votes);
+    let runners=0;
+    for (let x in votes){
+        if (votes[x]+k>leader){
+            runners++;
+        }
+    }
+    return runners;
 }
 
 let votes=[2,3,5,2];
