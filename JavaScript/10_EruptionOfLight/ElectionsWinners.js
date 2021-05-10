c97abe2eb317c6801708abdb086fea92c7ef60fe
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-10 17:44:15 -0400

    Need to rewrite to deal with edge case.

--- JavaScript/10_EruptionOfLight/ElectionsWinners.js
+++ JavaScript/10_EruptionOfLight/ElectionsWinners.js
@@ -1,15 +1,29 @@
 function electionsWinners(votes, k) {
-    let leader=Math.max(...votes);
-    let runners=0;
-    for (let x in votes){
-        if (votes[x]+k>leader){
-            runners++;
+    let curLeader=0
+
+    let curMax=-1;
+    for (let x=0;x<votes.length;x++){
+        if (votes[x]>curMax){
+            curMax=votes[x];
+            curLeader=[x];
+        } else if (votes[x]==curMax){
+            curLeader.push(votes[x])
         }
     }
-    return runners;
+    return curLeader;
+    // let runners=0;
+    // for (let x in votes){
+    //     if (votes[x]+k>leader){
+    //         runners++;
+    //     }
+    // }
+    // return runners;
 }
 
 let votes=[2,3,5,2];
 let k=3;
 
+console.log(electionsWinners(votes,k));
+votes=[2,3,5,5,1];
+k=0;
 console.log(electionsWinners(votes,k));
