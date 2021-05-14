7612c4e5abdc2947773b92ba5ad69e326b80c2e6
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-14 14:12:40 -0400

    All tests passed

--- JavaScript/10_EruptionOfLight/IsMAC48Address.js
+++ JavaScript/10_EruptionOfLight/IsMAC48Address.js
@@ -1,5 +1,5 @@
 function isMAC48Address(inputString) {
-    if(inputString.match(/([A-F0-9]{2})(-[A-F0-9]{2}){5}/)){
+    if(inputString.match(/^([A-F0-9]{2})(-[A-F0-9]{2}){5}$/)){
         return true;
     } else {
         return false;
