b0fd73a8c786e00daa330030b0c3e80c9fb3ab29
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-14 14:09:11 -0400

    Match needs to be full string

--- JavaScript/10_EruptionOfLight/IsMAC48Address.js
+++ JavaScript/10_EruptionOfLight/IsMAC48Address.js
@@ -1,4 +1,14 @@
 function isMAC48Address(inputString) {
-    return inputString.match(/([A-F0-9]{2})(-[A-F0-9]{2}){5}/)
+    if(inputString.match(/([A-F0-9]{2})(-[A-F0-9]{2}){5}/)){
+        return true;
+    } else {
+        return false;
+    }
 }
 
+let is ="00-1B-63-84-45-E6";
+console.log(isMAC48Address(is));
+is="Z1-1B-63-84-45-E6"
+console.log(isMAC48Address(is));
+is="not a MAC-48 address"
+console.log(isMAC48Address(is));
