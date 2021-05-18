d783b5158b7417fec9bf3d1b26a2152677633862
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-18 12:48:44 -0400

    Should encode single characters with number

--- JavaScript/11_RainbowOfClarity/LineEncoding.js
+++ JavaScript/11_RainbowOfClarity/LineEncoding.js
@@ -1,3 +1,20 @@
 function lineEncoding(s) {
-
+    let cur="";
+    let cnt=0;
+    let retVal="";
+    for (let x in s){
+        if (cur=="") cur=s[x];
+        if (cur==s[x]){
+            cnt++;
+        } else {
+            retVal+=cnt+cur;
+            cnt=1;
+            cur=s[x];
+        }
+    }
+    retVal+=cnt+cur;
+    return retVal;
 }
+
+let s = "aabbbc";
+console.log(lineEncoding(s));
