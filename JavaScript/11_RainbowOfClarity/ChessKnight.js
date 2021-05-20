9380b745492eafd9524e0adee1ef1e77e8e9be28
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-20 16:50:10 -0400

    All possible moves calculated

--- JavaScript/11_RainbowOfClarity/ChessKnight.js
+++ JavaScript/11_RainbowOfClarity/ChessKnight.js
@@ -1,14 +1,22 @@
 function chessKnight(cell) {
-    //2 Steps
-    console.log(String.fromCharCode(cell[0].charCodeAt()+2));
-    console.log(String.fromCharCode(cell[0].charCodeAt()-2));
-    console.log(String.fromCharCode(cell[1].charCodeAt()+2));
-    console.log(String.fromCharCode(cell[1].charCodeAt()-2));
-    //1 Step
-    console.log(String.fromCharCode(cell[0].charCodeAt()+1));
-    console.log(String.fromCharCode(cell[0].charCodeAt()-1));
-    console.log(String.fromCharCode(cell[1].charCodeAt()+1));
-    console.log(String.fromCharCode(cell[1].charCodeAt()-1));
+    let retVal=0;
+    let poss=[];
+
+    poss.push(String.fromCharCode(cell[0].charCodeAt()+2)+String.fromCharCode(cell[1].charCodeAt()+1))
+    poss.push(String.fromCharCode(cell[0].charCodeAt()+2)+String.fromCharCode(cell[1].charCodeAt()-1))
+    
+    poss.push(String.fromCharCode(cell[0].charCodeAt()-2)+String.fromCharCode(cell[1].charCodeAt()+1))
+    poss.push(String.fromCharCode(cell[0].charCodeAt()-2)+String.fromCharCode(cell[1].charCodeAt()-1))
+    
+    poss.push(String.fromCharCode(cell[0].charCodeAt()+1)+String.fromCharCode(cell[1].charCodeAt()-2))
+    poss.push(String.fromCharCode(cell[0].charCodeAt()-1)+String.fromCharCode(cell[1].charCodeAt()-2))
+    
+    poss.push(String.fromCharCode(cell[0].charCodeAt()+1)+String.fromCharCode(cell[1].charCodeAt()+2))
+    poss.push(String.fromCharCode(cell[0].charCodeAt()-1)+String.fromCharCode(cell[1].charCodeAt()+2))
+
+
+    
+    return retVal;
 
 }
 
