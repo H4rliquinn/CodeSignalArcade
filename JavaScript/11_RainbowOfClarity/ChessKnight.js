55c817ed1ced1f7ddd4f0fddfc60b1c0f1830d8d
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-20 16:53:07 -0400

    Valid moves identified All tests passed

--- JavaScript/11_RainbowOfClarity/ChessKnight.js
+++ JavaScript/11_RainbowOfClarity/ChessKnight.js
@@ -14,11 +14,14 @@ function chessKnight(cell) {
     poss.push(String.fromCharCode(cell[0].charCodeAt()+1)+String.fromCharCode(cell[1].charCodeAt()+2))
     poss.push(String.fromCharCode(cell[0].charCodeAt()-1)+String.fromCharCode(cell[1].charCodeAt()+2))
 
-
+    for (let x in poss){
+        if (poss[x].match(/[a-h][1-8]/)!=null) retVal++;
+    }
     
     return retVal;
-
 }
 
 let c="c2";
+console.log(chessKnight(c));
+c="a1";
 console.log(chessKnight(c));
