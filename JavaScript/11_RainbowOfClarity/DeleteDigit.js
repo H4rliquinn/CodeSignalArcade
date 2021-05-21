1d7b4a5adba82aaeb4e25f53cc26977273b4b7c4
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-21 12:46:57 -0400

    All tests passed

--- JavaScript/11_RainbowOfClarity/DeleteDigit.js
+++ JavaScript/11_RainbowOfClarity/DeleteDigit.js
@@ -1,7 +1,8 @@
 function deleteDigit(n) {
+    n+="";
     let max=-1;
-    for (let x in n){
-        console.log(x);
+    for (let x=0;x<n.length; x++){
+        max=Math.max(max,n.slice(0,x)+n.slice(x+1));
     }
     return max;
 }
