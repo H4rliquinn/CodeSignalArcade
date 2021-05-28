142ba16ed147ecbed1462b03d9010eab35be2207
Jason Sonnichsen <Harliquinn@gmail.com>
2021-05-28 11:04:59 -0400

    All tests passed

--- JavaScript/12_LandOfLogic/ValidTime.js
+++ JavaScript/12_LandOfLogic/ValidTime.js
@@ -1,6 +1,10 @@
 function validTime(time) {
     let nums=time.match(/\d\d/g);
-    return nums;
+    if (parseInt(nums[0])>=0 
+    && parseInt(nums[0])<24 
+    && parseInt(nums[1])>=0
+    && parseInt(nums[1])<60) return true;
+    return false;
 }
 
 let t="13:58";
