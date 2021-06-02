330443ed8ad4d636e7c6f344d93c0b95aa751dca
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-02 14:32:04 -0400

    Edge Case 1 text number not returning as Int. All tests passed.

--- JavaScript/12_LandOfLogic/SumUpNumbers.js
+++ JavaScript/12_LandOfLogic/SumUpNumbers.js
@@ -1,12 +1,18 @@
 function sumUpNumbers(inputString) {
-    let items=inputString.split(",");
+    let items=inputString.match(/\d+/g);
+    console.log(items);
     let total=0;
-    for (let x in items){
-        total+=parseInt(items[x].match(/\d+/));
-    }
-    
-    return total;
+    if (items!=null){
+        total=items.reduce((accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue));
+    } else return 0;
+    return parseInt(total);
 }
 
-let s="2 apples, 12 oranges";
+// let s="2 apples, 12 oranges";
+// console.log(sumUpNumbers(s));
+// s="No valid items needed 20 56 200";
+// console.log(sumUpNumbers(s));
+// s="No numbers";
+// console.log(sumUpNumbers(s));
+s="123450";
 console.log(sumUpNumbers(s));
