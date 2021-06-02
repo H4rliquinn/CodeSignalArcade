0677fdb8df2f40be2f4f2f56981e09a568b6a3ab
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-02 14:18:07 -0400

    Edge case for no items not handled

--- JavaScript/12_LandOfLogic/SumUpNumbers.js
+++ JavaScript/12_LandOfLogic/SumUpNumbers.js
@@ -1,6 +1,11 @@
 function sumUpNumbers(inputString) {
     let items=inputString.split(",");
-    return items;
+    let total=0;
+    for (let x in items){
+        total+=parseInt(items[x].match(/\d+/));
+    }
+    
+    return total;
 }
 
 let s="2 apples, 12 oranges";
