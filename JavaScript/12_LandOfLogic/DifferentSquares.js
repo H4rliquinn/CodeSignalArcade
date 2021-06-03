09dfb79baea6f3e49bebf09ba46effab84366ee5
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-03 15:46:54 -0400

    Add unique squares to library Count and return squares All tests passed

--- JavaScript/12_LandOfLogic/DifferentSquares.js
+++ JavaScript/12_LandOfLogic/DifferentSquares.js
@@ -1,9 +1,16 @@
 function differentSquares(matrix) {
+    let lib={};
+    let retVal=0;
     for (let y=0;y<matrix.length-1;y++){
         for (let x=0;x<matrix[y].length-1;x++){
-            console.log(matrix[y][x]);
+            let square=""+matrix[y][x]+matrix[y][x+1]+matrix[y+1][x]+matrix[y+1][x+1]
+            if (lib[square]==null){
+                lib[square]=1;
+                retVal++;
+            }
         }
     }
+    return retVal
 }
 
 let matrix = [[1, 2, 1],
