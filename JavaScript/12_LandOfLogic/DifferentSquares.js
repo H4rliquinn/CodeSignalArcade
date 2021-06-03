4591b211c76fabd68ddaf11d907246ce0dede221
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-03 15:40:55 -0400

    Iterate through matrix

--- JavaScript/12_LandOfLogic/DifferentSquares.js
+++ JavaScript/12_LandOfLogic/DifferentSquares.js
@@ -1,3 +1,15 @@
 function differentSquares(matrix) {
-
+    for (let y=0;y<matrix.length-1;y++){
+        for (let x=0;x<matrix[y].length-1;x++){
+            console.log(matrix[y][x]);
+        }
+    }
 }
+
+let matrix = [[1, 2, 1],
+[2, 2, 2],
+[2, 2, 2],
+[1, 2, 3],
+[2, 2, 1]]
+
+console.log(differentSquares(matrix));
