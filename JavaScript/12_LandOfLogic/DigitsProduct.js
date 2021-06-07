de4441bf5e2883c918a5216980fc3b9bb3c87cb1
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-07 12:44:39 -0400

    Adding digits

--- JavaScript/12_LandOfLogic/DigitsProduct.js
+++ JavaScript/12_LandOfLogic/DigitsProduct.js
@@ -1,6 +1,13 @@
 function digitsProduct(product) {
     if (product<10) return product;
+    let pr=product.toString().split("");
+    let retVal=0;
+    for (let x in pr){
+        console.log(pr[x]);
+        retVal+=parseInt(pr[x]);
+    }
+    return retVal;
 }
 
-let p=8;
+let p=12;
 console.log(digitsProduct(p));
