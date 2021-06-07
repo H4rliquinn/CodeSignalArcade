4e65e09ef9924796b750c5129a4b19fad60f0ff5
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-07 12:53:34 -0400

    6 Tests passed. Kept number too low.

--- JavaScript/12_LandOfLogic/DigitsProduct.js
+++ JavaScript/12_LandOfLogic/DigitsProduct.js
@@ -1,12 +1,15 @@
 function digitsProduct(product) {
     if (product<10) return product;
-    let pr=product.toString().split("");
-    let retVal=0;
-    for (let x in pr){
-        console.log(pr[x]);
-        retVal+=parseInt(pr[x]);
+    let total=1;
+    for (let y=0;y<601;y++){
+        let pr=y.toString().split("");
+        for (let x in pr){
+            total*=parseInt(pr[x]);
+        }
+        if (total==product) return y;
+        total=1;
     }
-    return retVal;
+    return -1;
 }
 
 let p=12;
