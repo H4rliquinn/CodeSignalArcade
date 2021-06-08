804ec39d86e62292b36d8f0b16ba4a282dfcd924
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-08 15:52:53 -0400

    Overlap on naming

--- JavaScript/12_LandOfLogic/FileNaming.js
+++ JavaScript/12_LandOfLogic/FileNaming.js
@@ -1,3 +1,19 @@
 function fileNaming(names) {
+    let lib={};
+    let retArray=[];
 
+    for (let x in names){
+        if (lib[names[x]]!=null){
+            retArray.push(names[x]+"("+lib[names[x]]+")");
+            lib[names[x]]++;
+        } else {
+            retArray.push(names[x])
+            lib[names[x]]=1
+        }
+    }
+    console.log(lib);
+    return retArray;
 }
+
+let n=["doc", "doc", "image", "doc(1)", "doc"];
+console.log(fileNaming(n));
