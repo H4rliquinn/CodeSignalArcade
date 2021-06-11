51cd41627e14f441c19d8e07b29898cb0b95beef
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-11 13:45:14 -0400

    Converts 1 letter

--- JavaScript/12_LandOfLogic/MessageFromBinaryCode.js
+++ JavaScript/12_LandOfLogic/MessageFromBinaryCode.js
@@ -1,3 +1,10 @@
 function messageFromBinaryCode(code) {
-
+    retVal+=getLetterFromBinary(code);
 }
+
+function getLetterFromBinary(byte){
+    return String.fromCharCode(parseInt(byte,2).toString(10));
+}
+let c="01001000";
+//c="010010000110010101101100011011000110111100100001";
+console.log(messageFromBinaryCode(c));
