b226c49e247555590c1cbd75bbeff1df2c89b896
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-14 13:42:29 -0400

    Break down string into bytes

--- JavaScript/12_LandOfLogic/MessageFromBinaryCode.js
+++ JavaScript/12_LandOfLogic/MessageFromBinaryCode.js
@@ -1,10 +1,20 @@
 function messageFromBinaryCode(code) {
-    retVal+=getLetterFromBinary(code);
+    let retVal="";
+    let pos=0;
+    while (pos<code.length){
+        console.log(code.slice);
+
+        pos+=8;
+    }
+    // retVal+=getLetterFromBinary(code);
+
+
+    return retVal
 }
 
 function getLetterFromBinary(byte){
     return String.fromCharCode(parseInt(byte,2).toString(10));
 }
 let c="01001000";
-//c="010010000110010101101100011011000110111100100001";
+c="010010000110010101101100011011000110111100100001";
 console.log(messageFromBinaryCode(c));
