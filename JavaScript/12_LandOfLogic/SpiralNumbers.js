c1ce88c951b0cb148c5c9559f628f94b8c0aa9e4
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-18 16:57:24 -0400

    Adds first line and switches direction

--- JavaScript/12_LandOfLogic/SpiralNumbers.js
+++ JavaScript/12_LandOfLogic/SpiralNumbers.js
@@ -1,16 +1,43 @@
 function spiralNumbers(n) {
-    (filler=[]).length=n;
-    filler.fill(null);
-    (retVal=[]).length=n;
-    retVal.fill(filler);
+    let retVal=[];
+    for (let x=0;x<n;x++){
+        retVal.push([])
+        for (let y=0;y<n;y++){
+            retVal[x].push(0);
+        }
+    }
 
+
+    let curr=1;
+    let location={x:-1,y:0}
     let len=retVal.length;
     let dir="R";
     let nxt={"R":"D","D":"L","L":"U","U":"R"};
-    console.log(nxt[dir]);
+    
+    while (curr<4){
+        
+        switch(dir){
+            case "R":
+                location["x"]++;
+                retVal[location["y"]][location["x"]]=curr;
+                curr++;
+                if (location["x"]==len-1){
+                    dir=nxt[dir];
+                }
+                break;
+            case "D":
+                break;
+            case "L":
+                break;
+            case "U":
+                break;
+        }
+    }
+
+    console.log(curr,dir);
     return retVal
 }
 
 
-let n=3;
-console.log(spiralNumbers(n));
+let num=3;
+console.log(spiralNumbers(num));
