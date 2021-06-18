27713c76c1180b46962b8377ff17aadb785490e1
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-18 16:39:22 -0400

    Set up spiral

--- JavaScript/12_LandOfLogic/SpiralNumbers.js
+++ JavaScript/12_LandOfLogic/SpiralNumbers.js
@@ -3,8 +3,14 @@ function spiralNumbers(n) {
     filler.fill(null);
     (retVal=[]).length=n;
     retVal.fill(filler);
+
+    let len=retVal.length;
+    let dir="R";
+    let nxt={"R":"D","D":"L","L":"U","U":"R"};
+    console.log(nxt[dir]);
     return retVal
 }
+
 
 let n=3;
 console.log(spiralNumbers(n));
