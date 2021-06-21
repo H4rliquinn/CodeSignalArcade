1a5ee2e759e1a293a3daab728b4e5d82fb169bd7
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-21 16:38:41 -0400

    possible solution

--- JavaScript/12_LandOfLogic/SpiralNumbers.js
+++ JavaScript/12_LandOfLogic/SpiralNumbers.js
@@ -10,34 +10,57 @@ function spiralNumbers(n) {
 
     let curr=1;
     let location={x:-1,y:0}
-    let len=retVal.length;
+    let min=0
+    let max=retVal.length;
     let dir="R";
     let nxt={"R":"D","D":"L","L":"U","U":"R"};
     
-    while (curr<4){
+    while (curr<10){
         
         switch(dir){
             case "R":
                 location["x"]++;
                 retVal[location["y"]][location["x"]]=curr;
                 curr++;
-                if (location["x"]==len-1){
+                if (location["x"]==max-1){
                     dir=nxt[dir];
                 }
                 break;
-            case "D":
-                break;
-            case "L":
+                case "D":
+                    location["y"]++;
+                    retVal[location["y"]][location["x"]]=curr;
+                    curr++;
+                    if (location["y"]==max-1){
+                        dir=nxt[dir];
+                        max--;
+                    }
+                    break;
+                case "L":
+                    location["x"]--;
+                    retVal[location["y"]][location["x"]]=curr;
+                    curr++;
+                    if (location["x"]==min){
+                        dir=nxt[dir];
+                    }
                 break;
             case "U":
+                location["y"]--;
+                retVal[location["y"]][location["x"]]=curr;
+                curr++;
+                if (location["y"]==min+1){
+                    dir=nxt[dir];
+                    min++;
+                }
                 break;
         }
     }
-
-    console.log(curr,dir);
+    // console.log(curr,dir);
     return retVal
 }
 
 
 let num=3;
-console.log(spiralNumbers(num));
+let ans=spiralNumbers(num);
+for (let col in ans){
+    console.log(ans[col]);
+}
