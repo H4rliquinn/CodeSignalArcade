9f5c4d1637143bf436c293e1e76cb29d19e30a8b
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-23 14:46:18 -0400

    Checked Columnsw

--- JavaScript/12_LandOfLogic/Sudoku.js
+++ JavaScript/12_LandOfLogic/Sudoku.js
@@ -1,20 +1,29 @@
 function sudoku(grid) {
-    let used=[];
+    let usedRow=[];
+    let usedCol={};
     //check lines
     for (let col in grid){
         for (let row in grid[col]){
-            if (used.includes(grid[col][row])){
+            if (usedRow.includes(grid[col][row])){
                 return false;
             } else {
-                used.push(grid[col][row]);
+                usedRow.push(grid[col][row]);
+            }
+            // console.log(row);
+            if (col==0){
+                usedCol[row]=[grid[col][row]];
+            } else {
+                if (usedCol[row].includes(grid[col][row])){
+                    return false;
+                } else{
+                    usedCol[row].push(grid[col][row])
+                }
             }
         }
-        used=[];
+        usedRow=[];
     }
-    console.log(used);
-    //check columns
 
-    
+
     //check squares
 
     return true;
@@ -32,14 +41,14 @@ let grid = [[1, 3, 2, 5, 4, 6, 9, 8, 7],
 
 console.log(sudoku(grid));
 
-grid = [[1, 3, 2, 5, 4, 6, 9, 2, 7],
-[4, 6, 5, 8, 7, 9, 3, 8, 1],
-[7, 9, 8, 2, 1, 3, 6, 5, 4],
-[9, 2, 1, 4, 3, 5, 8, 7, 6],
-[3, 5, 4, 7, 6, 8, 2, 1, 9],
-[6, 8, 7, 1, 9, 2, 5, 4, 3],
-[5, 7, 6, 9, 8, 1, 4, 3, 2],
-[2, 4, 3, 6, 5, 7, 1, 9, 8],
-[8, 1, 9, 3, 2, 4, 7, 6, 5]];
+// grid = [[1, 3, 2, 5, 4, 6, 9, 2, 7],
+// [4, 6, 5, 8, 7, 9, 3, 8, 1],
+// [7, 9, 8, 2, 1, 3, 6, 5, 4],
+// [9, 2, 1, 4, 3, 5, 8, 7, 6],
+// [3, 5, 4, 7, 6, 8, 2, 1, 9],
+// [6, 8, 7, 1, 9, 2, 5, 4, 3],
+// [5, 7, 6, 9, 8, 1, 4, 3, 2],
+// [2, 4, 3, 6, 5, 7, 1, 9, 8],
+// [8, 1, 9, 3, 2, 4, 7, 6, 5]];
 
-console.log(sudoku(grid));
+// console.log(sudoku(grid));
