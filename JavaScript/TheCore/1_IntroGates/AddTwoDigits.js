57b801f0d020ea7e2f78858b11839170df10a079
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-28 15:16:22 -0400

    Didn't I do this already? All tests passed.

--- JavaScript/TheCore/1_IntroGates/AddTwoDigits.js
+++ JavaScript/TheCore/1_IntroGates/AddTwoDigits.js
@@ -1,3 +1,11 @@
 function addTwoDigits(n) {
-
+    let retVal=0;
+    let digits=n.toString().split("");
+    for (let x in digits){
+        retVal+=parseInt(digits[x]);
+    }
+    return retVal;
 }
+
+let n=29;
+console.log(addTwoDigits(n));
