f96d66238eb5927941c391210ffe8142a5865322
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-28 15:19:31 -0400

    Why am I back in coding kindergarten? All tests passed

--- JavaScript/TheCore/1_IntroGates/LargestNumber.js
+++ JavaScript/TheCore/1_IntroGates/LargestNumber.js
@@ -1,3 +1,10 @@
 function largestNumber(n) {
-
+    let retVal="";
+    for (let x=0;x<n;x++){
+        retVal+="9";
+    }
+    return parseInt(retVal);
 }
+
+let n=2;
+console.log(largestNumber(n));
