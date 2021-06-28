e2002ae428154df67b17f76fc5f50b664b9c0aaa
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-28 15:30:33 -0400

    More straight math

--- JavaScript/TheCore/1_IntroGates/SeatsInTheater.js
+++ JavaScript/TheCore/1_IntroGates/SeatsInTheater.js
@@ -1,3 +1,10 @@
 function seatsInTheater(nCols, nRows, col, row) {
-
+    return (nCols-(col-1))*(nRows-row);
 }
+
+let nCols=16;
+let nRows=11;
+let col=5;
+let row=3;
+
+console.log(seatsInTheater(nCols,nRows,col,row));
