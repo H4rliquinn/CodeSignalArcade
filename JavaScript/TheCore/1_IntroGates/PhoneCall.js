46ea56cc1eb5f4f67e86d8b790abe9ba5c58e69b
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-29 15:41:12 -0400

    First minute dealt with

--- JavaScript/TheCore/1_IntroGates/PhoneCall.js
+++ JavaScript/TheCore/1_IntroGates/PhoneCall.js
@@ -1,3 +1,14 @@
 function phoneCall(min1, min2_10, min11, s) {
+    let time=0;
+    if (s>=min1){
+        time+=min1;
+        s-=min1;
+    }
 
 }
+
+let min1=3;
+let min2_10=1;
+let min11=2;
+let s=20;
+console.log(phoneCall(min1,min2_10,min11,s));
