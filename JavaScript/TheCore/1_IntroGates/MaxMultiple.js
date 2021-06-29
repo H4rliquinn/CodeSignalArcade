07b67a415b0735812fcb42d13368db9b6a8afadc
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-29 15:14:00 -0400

    Mor math All tests passed

--- JavaScript/TheCore/1_IntroGates/MaxMultiple.js
+++ JavaScript/TheCore/1_IntroGates/MaxMultiple.js
@@ -1,3 +1,9 @@
 function maxMultiple(divisor, bound) {
-
+    for (let x=bound;x>0;x--){
+        if (x%divisor==0) return x;
+    }
 }
+
+let div=3;
+let bound=10;
+console.log(maxMultiple(div,bound));
