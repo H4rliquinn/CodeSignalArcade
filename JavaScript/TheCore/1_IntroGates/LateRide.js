d5762da50b6fc110a0e9ecb3610d2d439734220c
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-29 15:32:21 -0400

    Added previous exercise as helper function Caclated hour and minute values

--- JavaScript/TheCore/1_IntroGates/LateRide.js
+++ JavaScript/TheCore/1_IntroGates/LateRide.js
@@ -1,3 +1,20 @@
 function lateRide(n) {
-
+    let hrs=parseInt(n/60);
+    let min=n%60;
+    console.log(hrs,min);
+    return 1
 }
+
+function addTwoDigits(n) {
+    let retVal=0;
+    let digits=n.toString().split("");
+    for (let x in digits){
+        retVal+=parseInt(digits[x]);
+    }
+    return retVal;
+}
+
+let n=240;
+console.log(lateRide(n));
+n=808;
+console.log(lateRide(n));
