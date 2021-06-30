face85238ad57a7564aebaa82e0e5c0cfa9623db
Jason Sonnichsen <Harliquinn@gmail.com>
2021-06-30 17:54:29 -0400

    All passed

--- JavaScript/TheCore/2_AtTheCrossroads/ReachNextLevel.js
+++ JavaScript/TheCore/2_AtTheCrossroads/ReachNextLevel.js
@@ -1,4 +1,12 @@
 function reachNextLevel(experience, threshold, reward) {
-
+    if (experience+reward>=threshold){
+        return true;
+    } else {
+        return false;
+    }
 }
 
+let exp=10;
+let thresh=15;
+let re=5;
+console.log(reachNextLevel(exp,thresh,re));
