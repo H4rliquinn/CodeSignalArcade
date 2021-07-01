aa8c19b0d75fa9cd2eadb988336affa844ffe83e
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-01 13:51:33 -0400

    Missing an edge case. 1 test failed

--- JavaScript/TheCore/2_AtTheCrossroads/KnapsackLight.js
+++ JavaScript/TheCore/2_AtTheCrossroads/KnapsackLight.js
@@ -1,11 +1,17 @@
 function knapsackLight(value1, weight1, value2, weight2, maxW) {
     let sack=0;
+    let curWeight=maxW;
     let items=[{v:value1,w:weight1,r:value1/weight1}];
     items.push({v:value2,w:weight2,r:value2/weight2});
     items.sort((a,b)=>{
         return b.r-a.r;
     })
-
+    for (let x in items){
+        if (items[x].w<=curWeight){
+            sack+=items[x].v;
+            curWeight-=items[x].w;
+        }
+    }
     return sack;
 }
 
@@ -14,4 +20,11 @@ let weight1=5;
 let val2=6;
 let weight2=4;
 let maxW=8;
+console.log(knapsackLight(val1,weight1,val2,weight2,maxW));
+maxW=9;
+console.log(knapsackLight(val1,weight1,val2,weight2,maxW));
+val1=5;
+weight1=3;
+val2=7;
+maxW=6;
 console.log(knapsackLight(val1,weight1,val2,weight2,maxW));
