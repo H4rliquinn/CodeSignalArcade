a68bf87e6241d1737d390cb72b8ad187cfcb027a
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-01 13:40:50 -0400

    Created array of items including relative value

--- JavaScript/TheCore/2_AtTheCrossroads/KnapsackLight.js
+++ JavaScript/TheCore/2_AtTheCrossroads/KnapsackLight.js
@@ -1,3 +1,14 @@
 function knapsackLight(value1, weight1, value2, weight2, maxW) {
+    let item1={v:value1,w:weight1,r:value1/weight1};
+    let item2={v:value2,w:weight2,r:value2/weight2};
 
+
+    return [item1,item2];
 }
+
+let val1=10;
+let weight1=5;
+let val2=6;
+let weight2=4;
+let maxW=8;
+console.log(knapsackLight(val1,weight1,val2,weight2,maxW));
