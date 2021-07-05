e91a061078469dd07f26229481653f657ce08333
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-05 14:44:46 -0400

    All tests passed

--- JavaScript/TheCore/2_AtTheCrossroads/IsInfiniteProcess.js
+++ JavaScript/TheCore/2_AtTheCrossroads/IsInfiniteProcess.js
@@ -1,3 +1,16 @@
 function isInfiniteProcess(a, b) {
-    
+    if ((b-a)%2!=0) {
+        return true;
+    }
+    if (b<a) {
+        return true;
+    }  
+    return false;
 }
+
+let a=2;
+let b=6;
+console.log(isInfiniteProcess(a,b));
+a=2;
+b=3;
+console.log(isInfiniteProcess(a,b));
