b10252f54d2d1421c5ee347cb5e13bea428bcc9c
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-05 14:29:56 -0400

    Basic logic test. All tests passed

--- JavaScript/TheCore/2_AtTheCrossroads/ExtraNumber.js
+++ JavaScript/TheCore/2_AtTheCrossroads/ExtraNumber.js
@@ -1,4 +1,13 @@
 function extraNumber(a, b, c) {
-
+    if (a==b){
+        return c;
+    } else if (a==c){
+        return b;
+    }
+    return a;
 }
 
+let a=2;
+let b=7;
+let c=2;
+console.log(extraNumber(a,b,c));
