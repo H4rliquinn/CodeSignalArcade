94afcfde5803725fc1be2215a9e24e34dce66264
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-06 12:51:08 -0400

    All tests passed

--- JavaScript/TheCore/2_AtTheCrossroads/ArithmeticExpression.js
+++ JavaScript/TheCore/2_AtTheCrossroads/ArithmeticExpression.js
@@ -1,3 +1,12 @@
 function arithmeticExpression(a, b, c) {
-
+    if (a+b==c) return true
+    else if (a-b==c) return true
+    else if (a*b==c) return true
+    else if (a/b==c) return true;
+    return false;
 }
+
+let a=2;
+let b=3;
+let c=5;
+console.log(arithmeticExpression(a,b,c));
