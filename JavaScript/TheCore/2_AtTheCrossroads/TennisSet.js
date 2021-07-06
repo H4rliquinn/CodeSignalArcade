3b3dd2b36fcfd14938998f588face15f605b03e8
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-06 13:05:57 -0400

    Almost, misunderstood the second case

--- JavaScript/TheCore/2_AtTheCrossroads/TennisSet.js
+++ JavaScript/TheCore/2_AtTheCrossroads/TennisSet.js
@@ -1,3 +1,18 @@
 function tennisSet(score1, score2) {
+    let max=Math.max(score1,score2);
+    let min=Math.min(score1,score2);
 
+    if (max==6 && min<5) return true;
+    else if (max>6 && max==min+2) return true;
+    return false;
 }
+
+let s1=3;
+let s2=6;
+console.log(tennisSet(s1,s2));
+s1=8;
+s2=5;
+console.log(tennisSet(s1,s2));
+s1=6;
+s2=5;
+console.log(tennisSet(s1,s2));
