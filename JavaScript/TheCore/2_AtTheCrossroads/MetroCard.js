58cfdad7c623921e0b4a1bd904f61938e008099a
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-08 13:06:13 -0400

    All tests passed

--- JavaScript/TheCore/2_AtTheCrossroads/MetroCard.js
+++ JavaScript/TheCore/2_AtTheCrossroads/MetroCard.js
@@ -1,3 +1,9 @@
 function metroCard(lastNumberOfDays) {
-
+    let retVal=[];
+    if (lastNumberOfDays==31) return [28,30,31];
+    if (lastNumberOfDays==28) return [31];
+    return [31];
 }
+
+let d=30;
+console.log(metroCard(d));
