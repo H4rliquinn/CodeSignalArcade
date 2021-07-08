291d0daa5c5240f60a0fe064bfac5673a5ffea5e
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-08 13:27:17 -0400

    All tests passed

--- JavaScript/TheCore/3_CornerOf0sAnd1s/KillKthBit.js
+++ JavaScript/TheCore/3_CornerOf0sAnd1s/KillKthBit.js
@@ -1,3 +1,9 @@
 function killKthBit(n, k) {
-    return ...;
+    let mask=1<<k-1;
+    // console.log(mask.toString(2),~mask.toString(2));
+    return n&=~(1<<k-1);
   }
+
+  let n=37;
+  let k=3;
+  console.log(killKthBit(n,k));
