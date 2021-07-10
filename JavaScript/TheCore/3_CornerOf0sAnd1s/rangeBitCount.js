dfc7b6b1de03cf83da38a6d4c29a529ff3187984
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-09 20:09:48 -0400

    Range Array created

--- JavaScript/TheCore/3_CornerOf0sAnd1s/rangeBitCount.js
+++ JavaScript/TheCore/3_CornerOf0sAnd1s/rangeBitCount.js
@@ -1,3 +1,14 @@
 function rangeBitCount(a, b) {
-
+    let cur=a;
+    let rangeArray=[cur];
+    while (cur<b) {
+        cur++;
+        rangeArray.push(cur);
+    }
+    
+    return rangeArray;
 }
+
+let a=2;
+let b=7;
+console.log(rangeBitCount(a,b));
