130071e188d1ea452a5d1cca07839cfc621738e2
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-09 20:01:17 -0400

    All tests passed

--- JavaScript/TheCore/3_CornerOf0sAnd1s/ArrayPacking.js
+++ JavaScript/TheCore/3_CornerOf0sAnd1s/ArrayPacking.js
@@ -1,3 +1,15 @@
 function arrayPacking(a) {
-
+    let retVal="";
+    for (let x in a){
+        
+        let bin=a[x].toString(2);
+        while (bin.length<8){
+            bin="0"+bin;
+        }
+        retVal=bin+retVal;
+    }
+    return parseInt(retVal,2);
 }
+
+let a=[24,85,0];
+console.log(arrayPacking(a));
