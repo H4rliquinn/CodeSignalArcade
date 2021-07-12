cb46704b6af85573810235304620c66aa8802b3b
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-12 13:10:05 -0400

    All tests passed

--- JavaScript/TheCore/3_CornerOf0sAnd1s/MirrorBits.js
+++ JavaScript/TheCore/3_CornerOf0sAnd1s/MirrorBits.js
@@ -1,8 +1,13 @@
 function mirrorBits(a) {
     let org=a.toString(2).split("");
+    let len=org.length;
     let retVal="";
 
-    return org;
+    console.log(org);
+    for (let x=0;x<len;x++){
+        retVal+=org.pop();
+    }
+    return parseInt(retVal,2);
 }
 
 let a=97;
