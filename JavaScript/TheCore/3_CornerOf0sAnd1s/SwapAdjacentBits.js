2d10a6b20d16494711c36074b918bbcc138c32b3
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-14 17:19:39 -0400

    Had to look up help on this one. Complex, but I get it.

--- JavaScript/TheCore/3_CornerOf0sAnd1s/SwapAdjacentBits.js
+++ JavaScript/TheCore/3_CornerOf0sAnd1s/SwapAdjacentBits.js
@@ -1,5 +1,13 @@
 function swapAdjacentBits(n) {
 
+    return parseInt(
+        ((n.toString(2).length % 2 ? "0" : "") + n.toString(2))
+          .match(/\d{2}/g).map(x =>
+            x.split("").reverse().join("")
+          ).join("")
+          ,
+        2
+      );
   }
 
   let n=13;
