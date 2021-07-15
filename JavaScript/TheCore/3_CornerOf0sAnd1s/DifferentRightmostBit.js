83462cc3b7036c4ee65c42c9aac435f8fa7076d7
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-15 12:36:25 -0400

    All tests passed

--- JavaScript/TheCore/3_CornerOf0sAnd1s/DifferentRightmostBit.js
+++ JavaScript/TheCore/3_CornerOf0sAnd1s/DifferentRightmostBit.js
@@ -1,3 +1,15 @@
 function differentRightmostBit(n, m) {
-    return ...;
+    return (
+        1 <<
+        (n ^ m)
+          .toString(2)
+          .split("")
+          .reverse()
+          .join("")
+          .indexOf("1")
+      );
   }
+
+let n=11;
+let m=13;
+console.log(differentRightmostBit(n,m));
