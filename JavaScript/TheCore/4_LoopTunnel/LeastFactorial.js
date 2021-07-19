227802e9a0dbdb5c95df5f97f197e4dc1a4e855f
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-19 17:04:16 -0400

    Edge case equal to not handled

--- JavaScript/TheCore/4_LoopTunnel/LeastFactorial.js
+++ JavaScript/TheCore/4_LoopTunnel/LeastFactorial.js
@@ -1,3 +1,10 @@
 function leastFactorial(n) {
-
+    let curr=1;
+    for (let x=1;x<120;x++){
+        curr*=x;
+        if (curr>n) return curr;
+    }
 }
+
+let n=17;
+console.log(leastFactorial(n));
