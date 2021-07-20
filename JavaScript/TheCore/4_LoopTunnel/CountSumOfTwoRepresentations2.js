bd75661b7f367d07768fd165d2c643ed74d230ff
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-20 15:07:46 -0400

    All pairs created

--- JavaScript/TheCore/4_LoopTunnel/CountSumOfTwoRepresentations2.js
+++ JavaScript/TheCore/4_LoopTunnel/CountSumOfTwoRepresentations2.js
@@ -1,10 +1,14 @@
 function countSumOfTwoRepresentations2(n, l, r) {
     let retVal=0;
-    // for (let x=0;x<=(r-l)/2;x++){
-    //     console.log(x);
-    // }
-    return ((r-l)/2)+1;
+    for (let x=l;x<=r;x++){
+        for (let y=x;y<=r;y++){
+            console.log(x,y);
+        }
+    }
+    
 }
 
 let n=6,l=2,r=4;
+console.log(countSumOfTwoRepresentations2(n,l,r));
+n=10;l=9;r=11;
 console.log(countSumOfTwoRepresentations2(n,l,r));
