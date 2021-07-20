4be7582add6672c4c241b1de36e135c88fd62bf7
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-20 14:52:15 -0400

    Quick solution based on assumption of at least one positive outcome. Edge cases failed.

---
+++ JavaScript/TheCore/4_LoopTunnel/CountSumOfTwoRepresentations2.js
@@ -0,0 +1,10 @@
+function countSumOfTwoRepresentations2(n, l, r) {
+    let retVal=0;
+    // for (let x=0;x<=(r-l)/2;x++){
+    //     console.log(x);
+    // }
+    return ((r-l)/2)+1;
+}
+
+let n=6,l=2,r=4;
+console.log(countSumOfTwoRepresentations2(n,l,r));
