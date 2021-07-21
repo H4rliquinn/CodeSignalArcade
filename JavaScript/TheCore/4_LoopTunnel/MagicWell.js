bfdc7f80413da859c335b66b694b52e9e726982d
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-21 13:53:13 -0400

    All tests passed

--- JavaScript/TheCore/4_LoopTunnel/MagicWell.js
+++ JavaScript/TheCore/4_LoopTunnel/MagicWell.js
@@ -1,3 +1,12 @@
 function magicalWell(a, b, n) {
-
+    let retVal=0;
+    for (let x=0;x<n;x++){
+        retVal+=a*b;
+        a++;
+        b++;
+    }
+    return retVal;
 }
+
+let a=1,b=2,n=2;
+console.log(magicalWell(a,b,n));
