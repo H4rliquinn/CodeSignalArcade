06d000079b94133d9ad6d24bd25bb3c8962a62ff
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-21 14:00:49 -0400

    Scan through command list

--- JavaScript/TheCore/4_LoopTunnel/LineUp.js
+++ JavaScript/TheCore/4_LoopTunnel/LineUp.js
@@ -1,3 +1,10 @@
 function lineUp(commands) {
-
+    let retVal=0;
+    let comArray=commands.split("");
+    for (let x in comArray){
+        console.log(comArray[x]);
+    }
 }
+
+let com="LLARL";
+console.log(lineUp(com));
