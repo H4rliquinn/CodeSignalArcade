8bd32ccd7d47a00bee84911d6df289a94becc72a
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-21 14:13:41 -0400

    Students turning properly based on commands

--- JavaScript/TheCore/4_LoopTunnel/LineUp.js
+++ JavaScript/TheCore/4_LoopTunnel/LineUp.js
@@ -1,8 +1,28 @@
 function lineUp(commands) {
     let retVal=0;
+    let students=["F","F"]; //Right, Wrong
+    //Commands
+    let lefts={F:"L",R:"F",L:"B",B:"R"};
+    let rights={F:"R",R:"B",L:"F",B:"L"};
+    let abouts={F:"B",R:"L",L:"R",B:"F"};
+
     let comArray=commands.split("");
     for (let x in comArray){
-        console.log(comArray[x]);
+        switch(comArray[x]){
+            case "L":
+                students[0]=lefts[students[0]];
+                students[1]=rights[students[1]];
+                break;
+            case "R":
+                students[0]=rights[students[0]];
+                students[1]=lefts[students[1]];    
+                break;
+            case "A":
+                students[0]=abouts[students[0]];
+                students[1]=abouts[students[1]];
+                break;
+        }
+        console.log(comArray[x],students);
     }
 }
 
