11cd915a790b5245d87199cfbe425ca1d12322cb
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-22 13:05:14 -0400

    All tests passed

--- JavaScript/TheCore/4_LoopTunnel/AdditionWithoutCarrying.js
+++ JavaScript/TheCore/4_LoopTunnel/AdditionWithoutCarrying.js
@@ -3,14 +3,13 @@ function additionWithoutCarrying(param1, param2) {
     let p1=param1.toString().split("").reverse();
     let p2=param2.toString().split("").reverse();
     for (let x=0;x<Math.max(p1.length,p2.length);x++){
-        
-        
+        retVal.push(oneDigit(p1[x]?p1[x]:0,p2[x]?p2[x]:0));
     }
-
+    return parseInt(retVal.reverse().join(""));
 }
 
 function oneDigit(a,b){
-    let c=a+b;
+    let c=parseInt(a)+parseInt(b);
     if (c>9){
         return c-10;
     } else return c;
