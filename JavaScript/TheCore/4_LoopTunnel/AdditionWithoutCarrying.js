036bb04b569da983eab72171effe31f86b71fec5
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-22 12:52:13 -0400

    Added helper funtion to 'forget' carried digit

--- JavaScript/TheCore/4_LoopTunnel/AdditionWithoutCarrying.js
+++ JavaScript/TheCore/4_LoopTunnel/AdditionWithoutCarrying.js
@@ -1,3 +1,15 @@
 function additionWithoutCarrying(param1, param2) {
-    
+    let p1=param1.toString().split("").reverse();
+    let p2=param2.toString().split("").reverse();
+
 }
+
+function oneDigit(a,b){
+    let c=a+b;
+    if (c>9){
+        return c-10;
+    } else return c;
+}
+
+let p1=456,p2=1734;
+console.log(additionWithoutCarrying(p1,p2));
