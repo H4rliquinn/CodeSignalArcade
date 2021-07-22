41dffbe071b8fd84e232bbd746b1bc34df084b26
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-22 13:13:13 -0400

    All tests passed

--- JavaScript/TheCore/4_LoopTunnel/AppleBoxes.js
+++ JavaScript/TheCore/4_LoopTunnel/AppleBoxes.js
@@ -1,3 +1,15 @@
 function appleBoxes(k) {
-
+    let yellow=0,red=0;
+    for (let x=1;x<=k;x++){
+        if (x%2==0){
+            red+=x*x;
+        } else {
+            yellow+=x*x;
+        }
+        console.log(red,yellow);
+    }
+    return red-yellow;
 }
+
+let k=5;
+console.log(appleBoxes(k));
