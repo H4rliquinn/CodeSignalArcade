fa13d26d4c1fb0e19ae762a937ab72d011431d98
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-26 12:36:51 -0400

    Instructions unclear, 5 tests passed

--- JavaScript/TheCore/4_LoopTunnel/IncreaseNumberRoundness.js
+++ JavaScript/TheCore/4_LoopTunnel/IncreaseNumberRoundness.js
@@ -1,3 +1,19 @@
 function increaseNumberRoundness(n) {
-
+    let num= n.toString().split("").reverse();
+    let found=false;
+    for (let x in num){
+        if (!found){
+            if (num[x]!=0) found=true;
+        } else {
+            if (num[x]=="0"){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    return false;
 }
+
+let n=902200100;
+console.log(increaseNumberRoundness(n));
