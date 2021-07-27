926fee874aab27287feee2486e2ae3156bb93f69
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-27 11:19:42 -0400

    All tests passed

--- JavaScript/TheCore/4_LoopTunnel/Rounders.js
+++ JavaScript/TheCore/4_LoopTunnel/Rounders.js
@@ -1,8 +1,19 @@
 function rounders(n) {
     let num= n.toString().split("").reverse();
+    let retVal=[];
+    let add=0;
     for (let x=0;x<num.length-1;x++){
-        console.log(num[x]);
+       num[x]= parseInt(num[x])+add;
+        if (num[x]>=5){
+            add=1;
+        } else {
+            add=0;
+        }
+        retVal.push("0");
     } 
+    retVal.push((parseInt(num.pop())+add).toString())
+    retVal.reverse();
+    return parseInt(retVal.join(""));
 }
 
 let n=1234;
