83c3995b954b6297a8f66064514a362a02065926
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-27 11:07:44 -0400

    Work through digits backwards

--- JavaScript/TheCore/4_LoopTunnel/Rounders.js
+++ JavaScript/TheCore/4_LoopTunnel/Rounders.js
@@ -1,3 +1,11 @@
 function rounders(n) {
-
+    let num= n.toString().split("").reverse();
+    for (let x=0;x<num.length-1;x++){
+        console.log(num[x]);
+    } 
 }
+
+let n=1234;
+console.log(rounders(n));
+n=1445;
+console.log(rounders(n));
