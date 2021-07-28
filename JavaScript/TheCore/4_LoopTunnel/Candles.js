a94905435a36fe3a8b4a9e9b5eeb361a9eef0fd5
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-28 16:38:15 -0400

    All tests passed

--- JavaScript/TheCore/4_LoopTunnel/Candles.js
+++ JavaScript/TheCore/4_LoopTunnel/Candles.js
@@ -1,3 +1,18 @@
 function candles(candlesNumber, makeNew) {
-
+    let leftovers=0;
+    let retVal=0;
+    while ((candlesNumber>0 || leftovers>=makeNew)){
+        if (leftovers>=makeNew){
+            candlesNumber+=parseInt(leftovers/makeNew);
+            leftovers=leftovers%makeNew;       
+        }
+        retVal+=candlesNumber;
+        leftovers+=candlesNumber;
+        candlesNumber=0;
+    }
+    return retVal;
 }
+
+let cn=5;
+let mn=2;
+console.log(candles(cn,mn));
