927e649fa28262e424b06c6e91d6583fc6d510ef
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-29 13:13:06 -0400

    Found a greatest common denominator function to use Problem requires some deep math.

--- JavaScript/TheCore/4_LoopTunnel/CountBlackCells.js
+++ JavaScript/TheCore/4_LoopTunnel/CountBlackCells.js
@@ -1,3 +1,15 @@
 function countBlackCells(n, m) {
 
-}
+    
+
+    function gcd(a, b) {
+      if (!b) {
+        return a;
+      }
+      return gcd(b, a % b);
+    }
+    return gcd(m, n) * s + (gcd(m, n) - 1) * 2;
+  }
+
+let n=3,m=4;
+console.log(countBlackCells(n,m));
