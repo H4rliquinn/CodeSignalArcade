efd4d90a97012cf939d0d06144079eb3908658c9
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-29 13:16:36 -0400

    Found largest value

--- JavaScript/TheCore/4_LoopTunnel/CountBlackCells.js
+++ JavaScript/TheCore/4_LoopTunnel/CountBlackCells.js
@@ -1,6 +1,13 @@
 function countBlackCells(n, m) {
+    let retVal=0;
+    let temp;
+    if (n > m) {
+        temp = n;
+        n = m;
+        m = temp;
+    }
 
-    
+
 
     function gcd(a, b) {
       if (!b) {
@@ -8,8 +15,10 @@ function countBlackCells(n, m) {
       }
       return gcd(b, a % b);
     }
-    return gcd(m, n) * s + (gcd(m, n) - 1) * 2;
+    return gcd(m, n) * retVal + (gcd(m, n) - 1) * 2;
   }
 
 let n=3,m=4;
+console.log(countBlackCells(n,m));
+n=3,m=3;
 console.log(countBlackCells(n,m));
