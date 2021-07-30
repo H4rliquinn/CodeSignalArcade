22a4f3bea301c4f9c8abc31d358b737b6445ee8e
Jason Sonnichsen <Harliquinn@gmail.com>
2021-07-30 13:15:03 -0400

    All tests passed

--- JavaScript/TheCore/5_ListForestEdge/ArrayReplace.js
+++ JavaScript/TheCore/5_ListForestEdge/ArrayReplace.js
@@ -1,5 +1,10 @@
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
-    
+    for (let x=0;x<inputArray.length;x++){
+        if (inputArray[x]==elemToReplace){
+            inputArray[x]=substitutionElem;
+        }
+    }
+    return inputArray;
 }
 
 let a=[1,2,1]
