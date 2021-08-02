3bdfd9ad877a210d4bd15d945774ca1bd6e32b49
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-02 13:15:14 -0400

    4/5 Tests passed. Caught on null edge case

--- JavaScript/TheCore/5_ListForestEdge/FirstReverseTry.js
+++ JavaScript/TheCore/5_ListForestEdge/FirstReverseTry.js
@@ -1,3 +1,9 @@
 function firstReverseTry(arr) {
-
+    let temp=arr[0];
+    arr[0]=arr[arr.length-1];
+    arr[arr.length-1]=temp;
+    return arr;
 }
+
+let a=[1,2,3,4,5];
+console.log(firstReverseTry(a));
