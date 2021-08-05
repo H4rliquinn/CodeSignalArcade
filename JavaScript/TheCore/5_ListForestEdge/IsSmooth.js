030b14e428ac3af0cd5ebeeae9ab85fd9f4b8360
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-05 12:14:07 -0400

    Found middle value

--- JavaScript/TheCore/5_ListForestEdge/IsSmooth.js
+++ JavaScript/TheCore/5_ListForestEdge/IsSmooth.js
@@ -1,3 +1,14 @@
 function isSmooth(arr) {
-
+    let middle;
+    if (arr.length%2==0){
+        middle=arr[parseInt(arr.length/2-1)]+arr[parseInt(arr.length/2)];
+    } else {
+        middle=arr[parseInt(arr.length/2)];
+    }
+    return middle;
 }
+
+let a=[7,2,2,5,10,7]
+console.log(isSmooth(a));
+a=[-5,-5,10]
+console.log(isSmooth(a));
