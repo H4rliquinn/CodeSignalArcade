a10600cc41bce37c45ec9ba72df365e622e2f054
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-06 12:21:49 -0400

    Code re-use from RemoveArrayPart and IsSmooth

--- JavaScript/TheCore/5_ListForestEdge/replaceMiddle.js
+++ JavaScript/TheCore/5_ListForestEdge/replaceMiddle.js
@@ -1,3 +1,9 @@
 function replaceMiddle(arr) {
-
+    let middle;
+    if (arr.length%2!=0){
+        middle=arr[parseInt(arr.length/2-1)]+arr[parseInt(arr.length/2)];
+        return [...arr.slice(0,parseInt(arr.length/2-2)),middle,...arr.slice(parseInt(arr.length/2)+1)];
+    }  
 }
+
+
