fabf5f5066d77c6422324cb39a4369851802e636
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-06 12:25:15 -0400

    All tests passed

--- JavaScript/TheCore/5_ListForestEdge/replaceMiddle.js
+++ JavaScript/TheCore/5_ListForestEdge/replaceMiddle.js
@@ -1,9 +1,12 @@
 function replaceMiddle(arr) {
     let middle;
-    if (arr.length%2!=0){
+    if (arr.length%2==0){
         middle=arr[parseInt(arr.length/2-1)]+arr[parseInt(arr.length/2)];
-        return [...arr.slice(0,parseInt(arr.length/2-2)),middle,...arr.slice(parseInt(arr.length/2)+1)];
-    }  
+        return [...arr.slice(0,parseInt(arr.length/2-1)),middle,...arr.slice(parseInt(arr.length/2)+1)];
+    }
+    return arr;
 }
 
+let a=[7,2,2,5,10,7]
+console.log(replaceMiddle(a));
 
