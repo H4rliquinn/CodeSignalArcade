19e29cd09ee08e8ac794185b9f6c7bc11dfc23e7
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-09 16:09:36 -0400

    All tests passed.

--- JavaScript/TheCore/5_ListForestEdge/MakeArrayConsecutive2.js
+++ JavaScript/TheCore/5_ListForestEdge/MakeArrayConsecutive2.js
@@ -1,3 +1,14 @@
 function makeArrayConsecutive2(statues) {
-
+    let min=Math.min(...statues);
+    let max=Math.max(...statues);
+    let retVal=0;
+    for (let x=min+1;x<max;x++){
+        if (!statues.includes(x)){
+            retVal++;
+        }
+    }
+    return retVal;
 }
+
+let a=[6,2,3,8];
+console.log(makeArrayConsecutive2(a));
