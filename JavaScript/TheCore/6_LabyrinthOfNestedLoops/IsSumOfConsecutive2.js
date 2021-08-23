83815930149f474a27d2dfee1b70c59d0906dc4e
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-23 16:30:05 -0400

    All tests passed

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/IsSumOfConsecutive2.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/IsSumOfConsecutive2.js
@@ -1,12 +1,17 @@
 function isSumOfConsecutive2(n) {
     let sumVal=0;
     for (x=1;x<n/2;x++){
-        let curSum=x;
-        while (currSum<n){
-            if (currSum==n) {
-                sumVal++;
+        let curSum=x,curX=x+1;
+        while (curSum<n){
+            curSum+=curX;
+            if (curSum==n) sumVal++;
+            curX++
         }
     }
-    return false;
+    return sumVal;
 }
 
+let n=9;
+console.log(isSumOfConsecutive2(n));
+n=8;
+console.log(isSumOfConsecutive2(n));
