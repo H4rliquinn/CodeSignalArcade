1950a38bbd863856f899574b87991708504cd8a4
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-24 13:22:18 -0400

    Create a library to reference when we're done

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/SquareDigitsSequence.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/SquareDigitsSequence.js
@@ -1,5 +1,12 @@
 function squareDigitsSequence(a0) {
-    
+    let lib={};
+    let counter=0;
+    let currentNum=a0;
+
+    while (lib[currentNum]==undefined){
+        lib[currentNum]=1;
+        
+    }
 }
 
 let n=16;
