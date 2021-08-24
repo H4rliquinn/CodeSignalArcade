1db7aa6139c66a7162c1977a346e1c4531460678
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-24 13:28:18 -0400

    All tests passed.

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/SquareDigitsSequence.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/SquareDigitsSequence.js
@@ -1,19 +1,21 @@
 function squareDigitsSequence(a0) {
     let lib={};
-    let counter=0;
+    let counter=1;
     let currentNum=a0;
 
-    let digits=currentNum.toString().split("");
-    currentNum=0;
-    for (let x in digits){
-        currentNum+=parseInt(digits[x])**2;
+    while (lib[currentNum]==undefined){
+        lib[currentNum]=1;
+        let digits=currentNum.toString().split("");
+        currentNum=0;
+        for (let x in digits){
+            currentNum+=parseInt(digits[x])**2;
+        }
+        counter++;
     }
-    // while (lib[currentNum]==undefined){
-    //     lib[currentNum]=1;
-
-    // }
-    return currentNum;
+    return counter;
 }
 
 let n=16;
+console.log(squareDigitsSequence(n));
+n=103;
 console.log(squareDigitsSequence(n));
