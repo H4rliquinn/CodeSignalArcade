2dc762072b962fe52ba37881f4c6dfe8ccb2fc88
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-25 13:04:26 -0400

    All tests passed

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/PagesNumberingWithInk.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/PagesNumberingWithInk.js
@@ -1,3 +1,18 @@
 function pagesNumberingWithInk(current, numberOfDigits) {
-
+    let digits=current.toString().split("");
+    while (numberOfDigits>=digits.length){
+        numberOfDigits-=digits.length;
+        current++
+        digits=current.toString().split("");
+    }
+    return current-1;
 }
+
+let c=1,num=5;
+console.log(pagesNumberingWithInk(c,num));
+c=21;
+num=5;
+console.log(pagesNumberingWithInk(c,num));
+c=8;
+num=4;
+console.log(pagesNumberingWithInk(c,num));
