8c45054a7280068dd03d5c584686b412a60e40f6
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-26 13:33:48 -0400

    Added Sum Function

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/ComfortableNumbers.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/ComfortableNumbers.js
@@ -1,3 +1,11 @@
 function comfortableNumbers(l, r) {
-
+    return sumNum(l);
 }
+
+function sumNum(n){
+    let arr=n.toString().split("");
+    return arr.reduce((accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue));
+}
+
+let l=10,r=12;
+console.log(comfortableNumbers(l,r));
