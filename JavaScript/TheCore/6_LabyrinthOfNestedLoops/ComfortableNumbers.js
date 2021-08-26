5e4405ab1245746246e050ce1668c9e981386d36
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-26 13:39:40 -0400

    Nested lops to test number pairs

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/ComfortableNumbers.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/ComfortableNumbers.js
@@ -1,7 +1,15 @@
 function comfortableNumbers(l, r) {
     let lib={};
+    let retVal=0;
+
     for (let x=l;x<=r;x++){
         lib[x]=[x-sumNum(x),x+sumNum(x)];
+    }
+
+    for (let x=l;x<r;x++){
+        for (let y=x+1;y<=r;y++){
+            console.log(x,y);
+        }
     }
 
     return lib;
