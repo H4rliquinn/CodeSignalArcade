9506a35df1c856bfe79e9a9f9722715e0cd0bb02
Jason Sonnichsen <Harliquinn@gmail.com>
2021-08-27 11:38:19 -0400

    Function to find number of divsors

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/WeakNumbers.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/WeakNumbers.js
@@ -1,3 +1,17 @@
 function weakNumbers(n) {
-
+    let divs=findDevisors(n)
+    return divs;
 }
+
+function findDevisors(n){
+    let retArr=[];
+    for (let x=1;x<=n;x++){
+        if (n%x==0){
+            retArr.push(x)
+        }
+    }
+    return retArr;
+}
+
+let a=8;
+console.log(weakNumbers(a));
