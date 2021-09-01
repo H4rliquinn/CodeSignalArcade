ddd0e285c37613fa3b66210301b6dce42e2c0901
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-01 15:15:57 -0400

    All tests passed

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/CrossworldFormation.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/CrossworldFormation.js
@@ -1,8 +1,4 @@
 function crosswordFormation(words) {
-    // let letters=[]
-    // for (let word in words){
-    //     letters.push(words[word].split(""));
-    // }
     let retVal=0;
     for (let i=0;i<words.length;i++){
         for (let j=0;j<words.length;j++){
@@ -11,18 +7,39 @@ function crosswordFormation(words) {
                     if (i!=j && i!=k && i!=l &&
                         j!=k && j!=l &&
                         k!=l){
-
-                            console.log(i,j,k,l);
+                            retVal+=wordCheck(words[i],words[j],words[k],words[l]);
                         }
                 }
             }
         }
     }
-    // return letters;
+    function wordCheck(a,b,c,d){
+        let count=0;
+        for (let a1=0;a1<a.length;a1++){
+            for (let a2=a1+2;a2<a.length;a2++){
+                for (b1=0;b1<b.length;b1++){
+                    for (let b2=b1+2;b2<b.length;b2++){
+                        for (let c1=0;c1<c.length;c1++){
+                            for (let d1=0;d1<d.length;d1++){
+                                let c2=c1+(a2-a1);
+                                let d2=d1+(b2-b1);
+                                if (c2<c.length && d2<d.length){
+                                    if (a.charAt(a1)==b.charAt(b1) &&
+                                        a.charAt(a2)==d.charAt(d1) &&
+                                        c.charAt(c1)==b.charAt(b2) &&
+                                        c.charAt(c2)==d.charAt(d2)) count++
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    return retVal;
 }
-
-
-
 
 let w=["crossword","square","formation","something"];
 console.log(crosswordFormation(w));
