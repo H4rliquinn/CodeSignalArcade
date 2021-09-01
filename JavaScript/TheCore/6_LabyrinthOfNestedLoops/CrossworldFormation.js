81fe1da767cc7dfff808e0ca1fa28def2a5e9ca9
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-01 15:00:42 -0400

    Scrolling through all possible word orders

--- JavaScript/TheCore/6_LabyrinthOfNestedLoops/CrossworldFormation.js
+++ JavaScript/TheCore/6_LabyrinthOfNestedLoops/CrossworldFormation.js
@@ -1,3 +1,28 @@
 function crosswordFormation(words) {
+    // let letters=[]
+    // for (let word in words){
+    //     letters.push(words[word].split(""));
+    // }
+    let retVal=0;
+    for (let i=0;i<words.length;i++){
+        for (let j=0;j<words.length;j++){
+            for (let k=0;k<words.length;k++){
+                for (let l=0;l<words.length;l++){
+                    if (i!=j && i!=k && i!=l &&
+                        j!=k && j!=l &&
+                        k!=l){
 
+                            console.log(i,j,k,l);
+                        }
+                }
+            }
+        }
+    }
+    // return letters;
 }
+
+
+
+
+let w=["crossword","square","formation","something"];
+console.log(crosswordFormation(w));
