907abe5ca3a866283d82f13e5178494accdbd079
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-13 15:35:53 -0400

    All tests passed

--- JavaScript/TheCore/7_BookMarket/IsTandemRepeat.js
+++ JavaScript/TheCore/7_BookMarket/IsTandemRepeat.js
@@ -1,3 +1,18 @@
 function isTandemRepeat(inputString) {
-
+    let add=inputString.length/2;
+    for (let x=0;x<add;x++){
+        if (inputString[x]==inputString[x+add]){
+            console.log(inputString[x]);
+        } else {
+            return false;
+        }
+    }
+    return true;
 }
+
+let is="tandemtandem";
+console.log(isTandemRepeat(is));
+is="qqq";
+console.log(isTandemRepeat(is));
+is="2w2ww";
+console.log(isTandemRepeat(is));
