027e446d399ef7b16ef1fcca8d06a0cac9aace86
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-14 13:15:17 -0400

    Made input all lowercase per usual Used previous CheckPalindrome function. All tests passed.

--- JavaScript/TheCore/7_BookMarket/IsCaseInsensitivePalindrome.js
+++ JavaScript/TheCore/7_BookMarket/IsCaseInsensitivePalindrome.js
@@ -1,3 +1,12 @@
 function isCaseInsensitivePalindrome(inputString) {
-
+    let lcInputString=inputString.toLowerCase();
+        for (x=0;x<parseInt(lcInputString.length/2);x++){
+            if (lcInputString[x]!==lcInputString[lcInputString.length-x-1]){
+                return false;
+            }
+        }
+        return true;
 }
+
+let is="AaBaa";
+console.log(isCaseInsensitivePalindrome(is));
