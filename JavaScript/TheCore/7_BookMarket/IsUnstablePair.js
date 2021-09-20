9d5600d41294d9b327e92a10980e56c6874dba9f
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-20 16:27:48 -0400

    Loop through shortest +1

--- JavaScript/TheCore/7_BookMarket/IsUnstablePair.js
+++ JavaScript/TheCore/7_BookMarket/IsUnstablePair.js
@@ -1,3 +1,10 @@
 function isUnstablePair(filename1, filename2) {
-
+    for (let x=0;x<Math.min(filename1.length,filename2.length)+1;x++){
+        console.log(filename1[x],filename2[x]);
+    }
 }
+
+let f1='aa',f2='AAB';
+isUnstablePair(f1,f2);
+// f1="A",f2=z;
+// isUnstablePair(f1,f2);
