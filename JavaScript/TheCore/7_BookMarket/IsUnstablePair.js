4721aa2cc8ba58b35a9681a51f5ff14d8c633366
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-21 11:23:48 -0400

    Second attempt

--- JavaScript/TheCore/7_BookMarket/IsUnstablePair.js
+++ JavaScript/TheCore/7_BookMarket/IsUnstablePair.js
@@ -1,16 +1,22 @@
 function isUnstablePair(filename1, filename2) {
+    let flag;
+    if (filename1<filename2) flag=true;
+
     for (let x=0;x<Math.min(filename1.length,filename2.length)+1;x++){
-        if (filename1[x]!=filename2[x]){
-            if (filename1[x]==undefined) return true;
-            if ((filename1[x]<filename2[x])!=(filename1[x].toUpperCase()<filename2[x])){
-               return false;
-            }
+        if ((filename2[x].toLowerCase()<filename1[x].toLowerCase())==flag){
+            return true;
+
+            // if (filename1[x]==undefined) return true;
+            // if ((filename1[x]<filename2[x])!=(filename1[x].toUpperCase()<filename2[x])){
+            //    return false;
+            // }
+        } else {
+            return false;
         }
     }
-    return false;
 }
 
 let f1='aa',f2='AAB';
 console.log(isUnstablePair(f1,f2));
-f1="A",f2="z";
+f1='A',f2='z';
 console.log(isUnstablePair(f1,f2));
