889f42da54a3f29ebc1eb9ea9cd57486fad5909c
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-22 13:43:50 -0400

    Created char counting helper function to parse both strings.

--- JavaScript/TheCore/8_MirrorLake/StringsConstruction.js
+++ JavaScript/TheCore/8_MirrorLake/StringsConstruction.js
@@ -1,14 +1,27 @@
 function stringsConstruction(a, b) {
+    let liba=charLib(a);
+    let libb=charLib(b);
+
+    // let max=0;
+    // for (let x in a){
+    //     if (lib[a[x]]<max){
+    //         max=lib[a[x]]
+    //     }
+    // }
+    return [liba,libb];
+}
+function charLib(s){
     let lib={};
-    for (let x in b){
-        if (lib[b[x]]!=undefined){
-            lib[b[x]]+=1;
+    for (let x in s){
+        if (lib[s[x]]!=undefined){
+            lib[s[x]]+=1;
         } else {
             lib[b[x]]=1;
         }
     }
     return lib;
 }
+
 
 let a="abc",b="abccba";
 console.log(stringsConstruction(a,b));
