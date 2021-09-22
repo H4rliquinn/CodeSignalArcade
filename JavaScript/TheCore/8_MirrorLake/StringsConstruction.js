5e9f2214809dc6b76f52608e912740ab15c13f71
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-22 13:36:52 -0400

    Created library of letters

--- JavaScript/TheCore/8_MirrorLake/StringsConstruction.js
+++ JavaScript/TheCore/8_MirrorLake/StringsConstruction.js
@@ -1,3 +1,14 @@
 function stringsConstruction(a, b) {
-
+    let lib={};
+    for (let x in b){
+        if (lib[b[x]]!=undefined){
+            lib[b[x]]+=1;
+        } else {
+            lib[b[x]]=1;
+        }
+    }
+    return lib;
 }
+
+let a="abc",b="abccba";
+console.log(stringsConstruction(a,b));
