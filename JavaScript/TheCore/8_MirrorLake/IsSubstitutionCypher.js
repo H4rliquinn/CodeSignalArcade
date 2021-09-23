bc9e69e92a7e054d27094980c38f2b4ffd4273cb
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-23 16:05:55 -0400

    Should work, needs tests

--- JavaScript/TheCore/8_MirrorLake/IsSubstitutionCypher.js
+++ JavaScript/TheCore/8_MirrorLake/IsSubstitutionCypher.js
@@ -1,3 +1,13 @@
 function isSubstitutionCipher(string1, string2) {
-
+ let lib={};
+ for (let x in string1){
+     let char=string1[x];
+     if (lib[char]==undefined){
+         lib[char]=string2[x];
+     } else if (lib[char]!=string2[x]){
+         return false;
+     }
+ }
+ return true;
 }
+
