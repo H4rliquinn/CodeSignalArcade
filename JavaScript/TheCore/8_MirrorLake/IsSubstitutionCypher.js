0d2b0e3c9b8aeaaefbd1c8bb231c05a69160bb69
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-23 16:19:38 -0400

    Separate cyphers calculated All tests passed

--- JavaScript/TheCore/8_MirrorLake/IsSubstitutionCypher.js
+++ JavaScript/TheCore/8_MirrorLake/IsSubstitutionCypher.js
@@ -1,17 +1,26 @@
 function isSubstitutionCipher(string1, string2) {
- let lib={};
- for (let x in string1){
-     let char=string1[x];
-     if (lib[char]==undefined){
-         lib[char]=string2[x];
-     } else if (lib[char]!=string2[x]){
-         return false;
-     }
- }
- return true;
+    let lib1={},lib2={};
+    for (let x in string1){
+        let char1=string1[x];
+        let char2=string2[x];
+        if (lib1[char1]==undefined){
+            lib1[char1]=char2;
+            
+        }
+        if (lib2[char2]==undefined){
+            lib2[char2]=char1;
+        }
+        if (lib1[char1]!=char2||lib2[char2]!=char1){
+            return false;
+        }
+    }
+    return true;
 }
 
 let s1="aacb",s2="aabc";
 console.log(isSubstitutionCipher(s1,s2));
 s2="acbb";
+console.log(isSubstitutionCipher(s1,s2));
+s1="aaxyaa";
+s2="aazzaa";
 console.log(isSubstitutionCipher(s1,s2));
