ffe75261d94f4ae1008971146d39b852b44cae98
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-27 14:56:19 -0400

    Added character library function

--- JavaScript/TheCore/8_MirrorLake/CreateAnagram.js
+++ JavaScript/TheCore/8_MirrorLake/CreateAnagram.js
@@ -1,3 +1,17 @@
 function createAnagram(s, t) {
-
+    
 }
+function charLib(s){
+    let lib={};
+    for (let x in s){
+        if (lib[s[x]]!=undefined){
+            lib[s[x]]+=1;
+        } else {
+            lib[s[x]]=1;
+        }
+    }
+    return lib;
+}
+
+let s = "AABAA", t = "BBAAA";
+console.log(createAnagram(s,t));
