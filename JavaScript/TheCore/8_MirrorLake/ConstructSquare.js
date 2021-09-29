137ca377ab4a025f67e2a05337bf7a579ff43a82
Jason Sonnichsen <Harliquinn@gmail.com>
2021-09-29 16:01:47 -0400

    Added array permutation function.

--- JavaScript/TheCore/8_MirrorLake/ConstructSquare.js
+++ JavaScript/TheCore/8_MirrorLake/ConstructSquare.js
@@ -1,3 +1,28 @@
 function constructSquare(s) {
-
+    let sa=s.split("");
+    sa=permutationArray(sa,[],[])
+    return sa;
 }
+
+function permutationArray(ia,curr,cache){
+    if (ia.length==1) {
+        curr.push(ia[0]);
+        cache.push(curr);
+        return cache;
+    }
+    
+    for (let i=0;i<ia.length;i++){
+        let newArray=[...ia];
+        let newCurr=[...curr];
+        newCurr.push(ia[i]);
+        newArray.splice(i,1);
+        cache=permutationArray(newArray,newCurr,cache)
+    }
+    return cache;
+}
+
+let s="ab";
+console.log(constructSquare(s));
+// let a=["a","b","c"];
+// let curr,cache;
+// console.log();
